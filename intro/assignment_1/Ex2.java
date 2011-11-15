f31a6cc0d4b33651cc1aa35782dffc29ccb4360d
Ory Band <oryband@gmail.com>
2011-11-15 02:30:31 +0200

    Ex2.java is finished (i think).

--- intro/assignment_1/Ex2.java
+++ intro/assignment_1/Ex2.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Ex2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int limit = input.nextInt();
 
+        boolean is_prime;
 
+        int c = 1;  // Counter. Initial value includes 2.
+        int s = 2;  // Sum.
+
+        if (limit != 2) {
+            for (int i=limit-1; i>2; i--) {
+                is_prime = true;
+
+                // Check if the current number is odd and is a prime.
+                if (i%2 != 0) {
+                    for (int k=i-1; k>2 && is_prime; k--) {
+                        if (i%k == 0) {
+                            is_prime = false;
+                        }
+                    }
+                // All Even numbers are non-prime.
+                // Note '2' has already been added to the sum & counter.
+                } else {
+                    is_prime = false;
+                }
+
+                // If the current number is a prime, add it to the sum and increase the counter.
+                if (is_prime)  {
+                    c++;
+                    s += i;
+                }
+            }
+        }
+
+        System.out.println( (float) s / (float) c );
+    }
 }
+
