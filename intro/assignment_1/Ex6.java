819149e3ca5eaef2b629d3104b5767cf1ca4bad4
Ory Band <oryband@gmail.com>
2011-11-17 13:49:34 +0200

    Ex6 finished.

--- intro/assignment_1/Ex6.java
+++ intro/assignment_1/Ex6.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+
 public class Ex6 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        int left;  // i%10.
+
+        int sum;        // Sum of digits.
+        int total = 0;  // Total sum of numbers.
+        
+        for (int i=1; i<=input; i++) {
+            left = i;
+            sum = 0;
+
+            do {
+                sum += left%10;
+                left = left/10;
+            } while (left != 0);
+
+            if (sum%7 == 0) {
+                total += i;
+            }
+        }
+
+        System.out.println(total);
     }
 }
