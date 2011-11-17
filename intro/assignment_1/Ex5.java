07d864c218d6be6a9641a5fa7f53136128bf15ba
Ory Band <oryband@gmail.com>
2011-11-17 13:19:44 +0200

    Ex5 finished.

--- intro/assignment_1/Ex5.java
+++ intro/assignment_1/Ex5.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Ex5 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        for (int i=2; i<input; i++) {
+            while(input%i == 0) {
+                System.out.println(i);
+
+                input = input/i;
+            }
+        }
+
+        if (input != 1) {
+            System.out.println(input);
+        }
     }
 }
