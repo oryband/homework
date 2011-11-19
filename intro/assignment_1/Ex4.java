80d0b7e67173cf736f1ad076d5260d5c5c62da53
Ory Band <oryband@gmail.com>
2011-11-19 22:30:28 +0200

    Made Ex4 more officient - no need to let C iterate from 1, but from 5, same for b&a - from 3.

--- intro/assignment_1/Ex4.java
+++ intro/assignment_1/Ex4.java
@@ -6,12 +6,12 @@ public class Ex4 {
 
         int input = scan.nextInt();
 
-        // Print all Pythagorean triples (a^2 + b^2 = c^2) until the limit entered as input.
+        // Print all Pythagorean triples (a^2 + b^2 = c^2) until the limit given as input.
         // Iterates over all c's, b's & a's, checks and prints triples.
-        for (int c=2; c<input; c++) {
-            for (int b=2; b<c; b++) {
-                for (int a=2; a<b; a++) {
-                    if (b*b + a*a == c*c) {
+        for (int c=5; c<input; c++) {
+            for (int b=3; b<c; b++) {
+                for (int a=3; a<b; a++) {
+                    if (a*a + b*b == c*c) {
                         System.out.println("(" + a + "," + b + "," + c + ") : " + a + "*" + a + " + " + b + "*" + b + " = " + c + "*" + c);
                     }
                 }
