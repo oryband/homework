d24c63066b72d4413c84acfa34b2ae41ebd03769
Ory Band <oryband@gmail.com>
2011-11-23 17:28:09 +0200

    assignment 2 part 1-1 work in progress (not yet finished).

--- intro/assignment_2/Part1.java
+++ intro/assignment_2/Part1.java
@@ -7,18 +7,45 @@ public class Part1 {
 	public static boolean increment(int[] vec, int base) {
         for (int i=0; i<vec.length; i++) {
             if (base - vec[i] != 1) {
+                for (int j=i; j<vec.length; j++) {
+                    if (vec[j] == base -1) {
+                        vec[j] = 0;
+                    } else {
+                        vec[j]++;
+                    }
+                }
+
                 return true;
             }
         }
 
+        nullify(vec);
 		return false;
 	}		
 
+    // Assigns 0's to all elements of a 1-dimensional array.
+    public static void nullify(int[] arr) {
+        int i;
+        for (i=0; i<arr.length; i++) {
+            arr[i] = 0;
+        }
+    }
+
 	// Task 1.2
 	public static boolean increment(int[][] matrix, int base) {
-		boolean succ=false;
-		// YOUR CODE HERE
-		return succ;
+        int i;
+        for (i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[i].length; j++) {
+                if (increment(matrix[i], base)) {
+                    return true;
+            }
+        }
+
+        for (i=0; i<matrix.length; i++) {
+            nullify(matrix[i]);
+        }
+
+        return false;
 	}
 	
 	/* ********************************** *
@@ -87,16 +114,16 @@ public class Part1 {
 	/* ********************************** *
 	 * *  Main you may want to use      * *
 	 * ********************************** */
-	public static void main(String[] args) {
-		//int n=4, m=4, c=3;
-		//long startTime=System.currentTimeMillis();
-		//int[][] sol=solver(n, m, c);
-        ////int[][] sol=randomSolver(n, m, c, n*m, n*m);
-		//long endTime=System.currentTimeMillis();
-		//System.out.println("Solution time : "+(endTime-startTime)+" ms");
-		//System.out.println("Solution found: "+(sol!=null));
-		//if(sol!=null) {
-			//System.out.println("Valid solution: "+isValidSolution(sol,c));
-		//}
-	}
+    public static void main(String[] args) {}
+        /*int n=4, m=4, c=3;
+        long startTime=System.currentTimeMillis();
+        int[][] sol=solver(n, m, c);
+        //int[][] sol=randomSolver(n, m, c, n*m, n*m);
+        long endTime=System.currentTimeMillis();
+        System.out.println("Solution time : "+(endTime-startTime)+" ms");
+        System.out.println("Solution found: "+(sol!=null));
+        if(sol!=null) {
+            System.out.println("Valid solution: "+isValidSolution(sol,c));
+        }
+	}*/
 }
