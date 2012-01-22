002736f0bbce528e3368dd7a1f8254b7d4bfc22a
Ory Band <oryband@gmail.com>
2012-01-22 22:37:48 +0200

    Finished Tester.java.

--- intro/assignment_5/Tester.java
+++ intro/assignment_5/Tester.java
@@ -4,62 +4,63 @@
  */
 public class Tester {
 
-	private static boolean testPassed = true;
-	private static int testNum = 0;
-	
-	/**
-	 * This entry function will test all classes created in this assignment.
-	 * @param args command line arguments
-	 */
-	public static void main(String[] args) {
-		
-		// Each function here should test a different class.
-		testPoint();
-		testTriangle();
-		testQuadrangle();
+    private static boolean testPassed = true;
+    private static int testNum = 0;
+    
+    /**
+     * This entry function will test all classes created in this assignment.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Each function here should test a different class.
+        testPoint();
+        testTriangle();
+        testQuadrangle();
+        testCircle();
         testShapesContainer();
-		
-		// Notifying the user that the code have passed all tests. 
-		if (testPassed) {
-			System.out.println("All " + testNum + " tests passed!");
-		}
-	}
+        
+        // Notifying the user that the code have passed all tests. 
+        if (testPassed) {
+            System.out.println("All " + testNum + " tests passed!");
+        }
+    }
 
-	/**
-	 * This utility function will count the number of times it was invoked. 
-	 * In addition, if a test fails the function will print the error message.  
-	 * @param exp The actual test condition
-	 * @param msg An error message, will be printed to the screen in case the test fails.
-	 */
-	private static void test(boolean exp, String msg) {
-		testNum++;
-		
-		if (!exp) {
-			testPassed = false;
-			System.out.println("Test " + testNum + " failed: "  + msg);
-		}
-	}
-	
-	/**
-	 * Checks the Point class.
-	 */
-	private static void testPoint() {
-		
-		Point p1 = new Point(10, 20);
+    /**
+     * This utility function will count the number of times it was invoked. 
+     * In addition, if a test fails the function will print the error message.  
+     * @param exp The actual test condition
+     * @param msg An error message, will be printed to the screen in case the test fails.
+     */
+    private static void test(boolean exp, String msg) {
+        testNum++;
+        
+        if (!exp) {
+            testPassed = false;
+            System.out.println("Test " + testNum + " failed: "  + msg);
+        }
+    }
+    
+    /**
+     * Checks the Point class.
+     */
+    private static void testPoint() {
+        
+        Point p1 = new Point(10, 20);
 
-		test(p1.getX() == 10, "X should be 10.");
-		test(p1.getY() == 20, "Y should be 20.");
-		
-		Point p2 = new Point(p1);
-		test(p2.equals(p1), "Point should be equal.");
-		test(p2 != p1, "Point should be different points.");
-		test(p2.distance(p1) == 0, "Distance should be zero.");
-		
-		p2.move(p1);
-		test(p2.getX() == 2*p1.getX(), "P2 x should be twice as P1 x.");
-		test(p2.getY() == 2*p1.getY(), "P2 y should be twice as P1 y.");
-		test(p2.distance(p1) == 22.360679774997898, "Distance should be 22.360679774997898");
-		test(!p2.equals(p1), "Point should not be equal.");
+        test(p1.getX() == 10, "X should be 10.");
+        test(p1.getY() == 20, "Y should be 20.");
+        
+        Point p2 = new Point(p1);
+        test(p2.equals(p1), "Point should be equal.");
+        test(p2 != p1, "Point should be different points.");
+        test(p2.distance(p1) == 0, "Distance should be zero.");
+        
+        p2.move(p1);
+        test(p2.getX() == 2*p1.getX(), "P2 x should be twice as P1 x.");
+        test(p2.getY() == 2*p1.getY(), "P2 y should be twice as P1 y.");
+        test(p2.distance(p1) == 22.360679774997898, "Distance should be 22.360679774997898");
+        test(!p2.equals(p1), "Point should not be equal.");
 
         Point p3 = new Point(p1.getX(), p1.getY());
         test(p3.equals(p1), "p3 should be equal to p1.");
@@ -71,12 +72,12 @@ public class Tester {
         Point p5 = new Point(0, 0);
         p5.move(p1.getX(), p1.getY());
         test(p5.equals(p1), "p5, should be equal to p1.");
-	}
+    }
 
-	/**
-	 * Checks the Triangle class.
-	 */
-	private static void testTriangle() {
+    /**
+     * Checks the Triangle class.
+     */
+    private static void testTriangle() {
         Point p1 = new Point(0,0),
               p2 = new Point(2,0),
               p3 = new Point(1,2);
@@ -111,12 +112,12 @@ public class Tester {
 
         Point p4 = new Point(1.5,1);
         test(t1.contains(p4) && t2.contains(p4) && t3.contains(p4), "p4 should be in t1, t2, t3.");
-	}
+    }
 
-	/**
-	 * Checks the Quadrangle class.
-	 */
-	private static void testQuadrangle() {
+    /**
+     * Checks the Quadrangle class.
+     */
+    private static void testQuadrangle() {
         Point p1 = new Point(0,0),
               p2 = new Point(1,0),
               p3 = new Point(1,1),
@@ -159,55 +160,112 @@ public class Tester {
 
         Point p5 = new Point(0.5,0.5);
         test(q1.contains(p4) && q2.contains(p4) && q3.contains(p4), "p5 should be in q1, q2, q3.");
-	}
+    }
 
-	private static void testShapesContainer() {
-		ShapesContainer c = new ShapesContainer();
+    private static void testCircle() {
+        Point p1 = new Point(0,0),
+              p2 = new Point(7,7.5);
 
-        test(c.getShapesNum() == 0, "Container should be empty.");
+        double r1 = 1,
+               r2 = 2.5;
+
+        Circle c1 = new Circle(p1, r1),
+               c2 = new Circle(p2, r2),
+               c3 = new Circle(c1);
+
+
+        test(c1.getCenter().equals(new Point(0,0)), "c1.center should be (0,0)");
+        test(c1.getRadius() > 0.9 && c1.getRadius() < 1.1, "c1.radius should be ~1.0");
+        test(c2.getCenter().equals(new Point(7,7.5)), "c2.center should be (7,7.5)");
+        test(c2.getRadius() > 2.4 && c2.getRadius() < 2.6, "c2.radius should be ~2.5");
+
+        test(c1.equals(c3), "c1 != c3");
+
+        //System.out.println("c1 per: " + c1.getPerimeter());
+        //System.out.println("c2 per: " + c2.getPerimeter());
+        //System.out.println("c3 per: " + c3.getPerimeter());
+
+        //System.out.println("c1 area: " + c1.getArea());
+        //System.out.println("c2 area: " + c2.getArea());
+        //System.out.println("c3 area: " + c3.getArea());
+
+        test(c1.getPerimeter() > 6 && c1.getPerimeter() < 7, "c1 perimeter should be ~6.");
+        test(c1.getArea() > 3 && c1.getArea() < 4, "c1 area should be ~3.");
+
+        test(c1.getPerimeter() == c3.getPerimeter(), "c1,c3's perimeter should be equal.");
+        test(c1.getArea() == c3.getArea(), "c1,c3's area should be equal.");
+
+        Point p3 = new Point(0,0.5);
+        test(c1.contains(p3) && ! c2.contains(p3) && c3.contains(p3), "p3 should be in c1 & c3 BUT NOT in c2");
+    }
+
+    private static void testShapesContainer() {
+        ShapesContainer c1 = new ShapesContainer();
+
+        test(c1.getShapesNum() == 0, "c1 should be empty.");
 
         Triangle t1 = new Triangle(new Point(0,0),
                                    new Point(2,0),
                                    new Point(1.5,2));
-        c.add(t1);
-        test(!c.add(t1), "t1 was added twice in a row!");
-        test(c.getShapesNum() == 1, "Container should be of length 1.");
+
+        //System.out.println("t1 perimeter: " + t1.getPerimeter());
+        //System.out.println("t1 area: " + t1.getArea());
+        //System.out.println();
+
+        c1.add(t1);
+
+        test( ! c1.add(t1), "t1 was added twice in a row!");
+        test(c1.getShapesNum() == 1, "c1 should be of length 1.");
 
         Triangle t2 = new Triangle(new Point(0,0),
-                                   new Point(5,0),
-                                   new Point(3,5));
-        c.add(t2);
+                                   new Point(6,0),
+                                   new Point(3,6));
+
+        //System.out.println("t2 perimeter: " + t2.getPerimeter());
+        //System.out.println("t2 area: " + t2.getArea());
+        //System.out.println();
+
+        c1.add(t2);
+
+        test(c1.getShapesNum() == 2, "c1 should be of length 2.");
 
         Quadrangle q1 = new Quadrangle(new Point(0,0),
                                        new Point(1,0),
                                        new Point(1,1),
                                        new Point(0,1));
-        c.add(q1);
 
-        ShapesContainer c2 = new ShapesContainer(c);
+        //System.out.println("q1 perimeter: " + q1.getPerimeter());
+        //System.out.println("q1 area: " + q1.getArea());
+        //System.out.println();
 
-        test(c.sumPerimeter() > 26 && c.sumPerimeter() < 27, "Sum of perimeters is wrong.");
-        test(c.sumArea() > 15 && c.sumArea() < 16, "Sum of areas is wrong.");
+        c1.add(q1);
 
-        test(c.getShapesNum() == 3, "Container should be of length 2.");
-        c.remove(0);
-        c.remove(t2);
-        test(c.getShapesNum() == 2, "Container should be of length 1.");
-        c.remove(t1);
-        test(c.getShapesNum() == 1, "Container should be empty.");
+        test(c1.getShapesNum() == 3, "c1 should be of length 3.");
 
-        test(c.sumPerimeter() > 3 && c.sumPerimeter() < 5, "Sum of perimeters is wrong.");
-        test(c.sumArea() > 0 && c.sumArea() < 2, "Sum of areas is wrong.");
+        ShapesContainer c2 = new ShapesContainer(c1);
+
+        test(c2.getShapesNum() == 3, "c2 should be of length 3.");
+
+        test(c1.sumPerimeter() > 29 && c1.sumPerimeter() < 30, "c1: Sum of perimeters is wrong.");
+        test(c1.sumArea() > 21 && c1.sumArea() < 22, "c1: Sum of areas is wrong.");
+
+        c1.remove(0);  // should be t2
+        c1.remove(t2); // should do nothing.
+
+        test(c1.getShapesNum() == 2, "c1 should be of length 2.");
+
+        c1.remove(t1);
+
+        test(c1.getShapesNum() == 1, "c1 should be of length 1.");
+
+        test(c1.sumPerimeter() > 3 && c1.sumPerimeter() < 5, "c1, after removal: Sum of perimeters is wrong.");
+        test(c1.sumArea() > 0 && c1.sumArea() < 2, "c1, after removal: Sum of areas is wrong.");
+
+
 
         c2.move(new Point(1,1));
 
-        test(c2.sumPerimeter() > 26 && c2.sumPerimeter() < 27, "Sum of perimeters 2 is wrong.");
-        test(c2.sumArea() > 15 && c2.sumArea() < 16, "Sum of areas 2 is wrong.");
-
-        test(c2.getShapesNum() == 3, "Container 2 should be of length 2.");
-        c2.remove(0);
-        test(c2.getShapesNum() == 2, "Container 2 should be of length 1.");
-        c2.remove(t1);
-        test(c2.getShapesNum() == 1, "Container 2 should be empty.");
-	}
+        test(c2.sumPerimeter() > 29 && c2.sumPerimeter() < 30, "c2: Sum of perimeters 2 is wrong.");
+        test(c2.sumArea() > 21 && c2.sumArea() < 22, "c2: Sum of areas 2 is wrong.");
+    }
 }
