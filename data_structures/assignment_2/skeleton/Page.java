c250961f4a18f910a912d5e5f74585cf492b0dcb
Ory Band <oryband@gmail.com>
2012-04-27 22:42:56 +0300

    Added hd index to Page.

--- data_structures/assignment_2/skeleton/Page.java
+++ data_structures/assignment_2/skeleton/Page.java
@@ -6,19 +6,22 @@
  */
 public class Page {
     public String data;
+    public int    index;  // Hard-disk index.
     public Page   next, prev;
 
     /**
-     * @param data String data.
-     * @param next The next (towards the last, newest) page in RAM.
-     * @param prev The previous (towdrds the first, oldest) page in RAM.
+     * @param data  String data.
+     * @param index Hard-disk index.
+     * @param next  The next (towards the last, newest) page in RAM.
+     * @param prev  The previous (towdrds the first, oldest) page in RAM.
      *
      * @return A new initialized Page object.
      */
-    public Page(String data, Page next, Page prev) {
-        this.data = new String(data);
-        this.next = next;
-        this.prev = prev;
+    public Page(String data, int index, Page next, Page prev) {
+        this.data  = new String(data);
+        this.index = index;
+        this.next  = next;
+        this.prev  = prev;
     }
 }
 
