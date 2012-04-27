2107276f919c70ce8f5adfe7484a7d636d6c14a0
Ory Band <oryband@gmail.com>
2012-04-27 21:23:29 +0300

    Implemented Page and RAM classes.

--- data_structures/assignment_2/skeleton/Page.java
+++ data_structures/assignment_2/skeleton/Page.java
@@ -1,6 +1,24 @@
-
+/**
+ * Implements a data page.
+ *
+ * @author Ory Band
+ * @version 1.0
+ */
 public class Page {
-	/*
-	 * Your code here
-	 */
+    public String data;
+    public Page   next, prev;
+
+    /**
+     * @param data String data.
+     * @param next The next (towards the last, newest) page in RAM.
+     * @param prev The previous (towdrds the first, oldest) page in RAM.
+     *
+     * @return a new initialized Page object.
+     */
+    public Page(String data, Page next, Page prev) {
+        this.data = new String(data);
+        this.next = next;
+        this.prev = prev;
+    }
 }
+
