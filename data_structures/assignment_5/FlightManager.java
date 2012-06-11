b8c751eafa1c3ee1ca5634efcb1459dc17b4bd06
Ory Band <oryband@gmail.com>
2012-06-12 00:50:33 +0300

    Working on stage 1 & 2 - still need to update AVL implementations so this will be ready.

--- data_structures/assignment_5/FlightManager.java
+++ data_structures/assignment_5/FlightManager.java
@@ -4,7 +4,7 @@
  * @author Liran Oz, Ory Band
  * @version 1.0
  */
-public class FlightManager{
+public class FlightManager {
     /**
      * Main hash function.
      *
@@ -51,104 +51,146 @@ public class FlightManager{
             temp;
 
         // Shift all digits.
-        while (n!=0) {
-            res  *= 10;      // Shift all digits left.
+        while (n != 0) {
+            res  *= 10;      // Shift all digits left and add a zero at the end (first digit).
             temp  = n % 10;  // Fetch left digit.
-            res  += temp;    // Insert it at the beginning.
-            n    /= 10;      // Removed (not shifted) leftmost digits.
+            res  += temp;    // Insert it at the beginning (where the zero was at).
+            n    /= 10;      // Remove (non-shifted) leftmost digits.
         }
 
         return res;
     }
 
+    /**
+     * @param path ID string read from file.
+     *
+     * @return ID list.
+     */
     private int[] getIds(String path) {
-        String s = Files.read("input1.dat");
+        String s = Files.read(path);
         String[] ss = s.split(",");
 
-        int l = ids.length;
+        int l = ss.length;
         int[] ids = new int[l];
 
         // Convert string IDs to int.
         for(int i=0; i<l; i++) {
-            ss[i] = s[i].trim();
+            ss[i] = ss[i].trim();
             ids[i] = Integer.parseInt(ss[i]);
         }
+
+        return ids;
     }
 
+    /**
+     * @param ids ID list.
+     *
+     * @return Initialized AVL array.
+     */
     private AvlTree[] buildAVLs(int[] ids) {
-        int l = ids.length;
-        AvlTree[] t = new AvlTree[l/3];
+        int l = ids.length,
+            al = l/3;
+
+        AvlTree[] t = new AvlTree[al];
 
         // Build tree.
-        int i;
+        int i, slot;
         for (i=0 ; i<l; i++) { 
-            t[ hash1(ids[i], l) ].insert(ids[i]);    
+            slot = hash1(ids[i], l);
+            t[slot].insert(ids[i]);    
         }
 
-        // Print row 1: Trees' height and size.
-        for(i=0; i < l/3 -1; i++) {
-            System.out.print(t[i].height + " " + t[i].size + " ");
+        // Print row 1 - Trees' height and size.
+        for(i=0; i < al -1; i++) {
+            System.out.print(t[i].height() + " " + t[i].size() + " ");  // TODO: Add height() / size() implementations.
         }
 
-        System.out.println(t[l/3 -1].height + " " + t[l/3 -1].size);
+        System.out.println(t[al -1].height() + " " + t[al -1].size());
     }
 
-    public static void stage1(String path) {
-        int ids[] = getIds(path);
-        AvlTree[] t = buildAVLs(ids);
+
+    /**
+     * @param trees Customer AVL trees array.
+     * @param registered Registered customers.
+     * @param arrivals Arrived customers - not necesseriarely registered.
+     */
+    private void processArrivals(AvlTree[] trees, int[] registered, int[] arrivals) {
+        int rl = registered.length,
+            al = arrivals.length;
+
+        // Build boardees and standby lists.
+        int[] boardees = new int[rl],
+              standby  = new int[al];
+
+        int c_boardees = 0,  // counters.
+            c_standby = 0,
+            steps = 0,       // Statistics calcs.
+
+            slot,            // Tree slot (index).
+            customer,        // ID.
+            i;
+
+        for (i=0; i < al; i++) {  // Load registered arrivals.
+            customer = arrivals[i];
+            slot = hash1(customer, rl);
+
+            int[] result = trees[slot] . find(customer);  // TODO: Fix this.
+            steps += 2 + result[0];  // Sum the steps required to find each arrival.
+
+            if ( result[1] ) {  // Load passenger if registered.
+                boardees[c_boardees++] = customer;
+            } else {  // Else put him on standby.
+                standby[c_standby++] = customer;
+            }
+        }
+
+        for (i=c_boardees; i<rl; i++) {  // Fill available standby seats.
+            boardees[i] = standby[i - c_boardees];
+        }
+
+        System.out.println(steps / al);  // Print row 2 - Average search-steps per arrival.
+        printSortedBoardees(boardees);   // Row 3.
     }
 
-        // lvl 2
-        String str2 = read_From_File_Example("input2.dat");//read the file to a string
-        String[] ids2 = str.split(",");//now you have an array of ids as strings (but they contain spaces
-        int N2 = ids2.length;//this is N - the amount of ids
-        int[] idsint2 = new int[N2]; 
-        for(int i = 0; i <N2 ;i++)
-        {
-            ids2[i] = ids2[i].trim();//remove spaces
-            idsint2[i]=Integer.parseInt(ids2[i]);
+
+    /**
+     * @param boardees Boardees list.
+     */
+    private void printSortedBoardees(int[] boardees) { 
+        int[] sorted = new int[boardees.length];
+
+        int   l = boardees.length;
+
+        // Deep-copy, so we won't affect the original boardees list.
+        int i;
+        for (i=0; i<l; i++) {
+            sorted[i] = boardees[i];
         }
-        int[] checkin= new int[N]; // the people on the flight
-        int[] spare= new int[N2];  // the waiting list.
-        int countercheckin=0;
-        int counterspare=0;
-        int steps=0;
-        for (i=0;i<N2;i=i+1){ // running on the array of the new people
-            steps=steps+2+reg[hs1(idsint2[i],N)].steps(idsint2[i]); // sum the steps for each person - using AvlTree.steps.
-            if (reg[hs1(idsint2[i],N)].find(idsint2[i])!=null) { // check if each person is in the flight.
-                checkin[countercheckin]=idsint2[i]; // if yes insert him to the flight
-                countercheckin=countercheckin+1; // add 1 to the counter,
-            }
-            else {
-                spare[counterspare]=idsint2[i]; // else add him to the spare array.
-                counterspare=counterspare+1; // add 1 to the counter.
-            }
+
+        Files.quickSort(sorted);  // TODO: Fix this/
+
+        for (i=0; i<l-1; i++) {
+            System.out.print(sorted[i] + " ");  // Row 3
         }
-        System.out.println(steps/N2); // row 2 - avg steps.
-        for (i=counterchecking;i<N;i=i+1){ // inserting other people to the plane.
-            checkin[i]=spare[i-countercheckin];
-        }
-        int[]checkinsort=new int[N]; // copying checking to sort it 
-        for (i=0;i<N;i=i+1)
-            checkinsort[i]=checkin[i];
-        quickSort(checkinsort); // sorting
-        for (i=0;i<N-1;i=i+1) // printing the people of this flight
-            System.out.print(checkinsort[i]+" "); //row 3
-        System.out.print(checkinsort[N-1]); //row 3
-        System.out.println(); 
-        lvl 3
+
+        System.out.println(sorted[l-1]);
+    }
 
 
+    public void processFlight(String registered_path, String arrivals_path) {
+        int[] registered = getIds(registered_path),
+              arrivals   = getIds(arrivals_path);
 
+        // Stage 1.
+        AvlTree[] trees = buildAVLs(registered);
 
+        // Stage 2
+        processArrivals(trees, registered, arrivals);
+    }
 
-
-
-
-            write_To_File_Example("output1.dat","number of ids = " + N);//write N to the output file
-        for(int i = 0; i <N ;i++)
-        {
-            write_To_File_Example("output1.dat","id number " + i + " : " + ids[i]);//write ids[i] to the output file
-        }
-    }  
+    //write_To_File_Example("output1.dat","number of ids = " + N);//write N to the output file
+    //for(int i = 0; i <N ;i++) {
+        //write_To_File_Example("output1.dat","id number " + i + " : " + ids[i]);//write ids[i] to the output file
+    //}
 }
+
