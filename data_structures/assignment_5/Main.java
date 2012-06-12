2f1b47274d2d7a53a0f72d2b30e9a9f54a8656cf
Ory Band <oryband@gmail.com>
2012-06-12 15:42:25 +0300

    Added Main

---
+++ data_structures/assignment_5/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String stage1 = "input1.dat";
+        String stage2 = "input2.dat";
+
+        FlightManager f = new FlightManager();
+        f.processFlight(stage1, stage2);
+    }
+}
+
