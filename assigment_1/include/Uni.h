7b3763a358e2db08e023ff3a02d9489f60c8e658
Ory Band <oryband@gmail.com>
2012-11-05 21:51:42 +0200

    Renamed all class source files to have Capital initial letter; Moved Student from .cpp to .h since there're barely any code; Added missing #includes;

--- assigment_1/include/Uni.h
+++ assigment_1/include/Uni.h
@@ -1,10 +1,19 @@
 #ifndef UNI_H
 #define UNI_H
 
+#include "Course.h"
+#include "Student.h"
+
 #include <string>
 
 
 class Uni {
+    public:
+        Uni(string coursesPath, string studentsPath);
+        ~Uni();
+
+        void assignStudents();
+        void printAssignment();
     private:
         vector<Course> courses;
         vector<Student> unassignedStudents;
@@ -12,12 +21,6 @@ class Uni {
         vector<Course>* readCourses();
         vector<Student>* readStudents();
 
-    public:
-        Uni(string coursesPath, string studentsPath);
-        ~Uni();
-
-        void assignStudents();
-        void printAssignment();
 };
 
 #endif
---
+++ assigment_1/src/Uni.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+//#include <vector>
+
+#include "Uni.h"
+
+using namespace std;
+
+
+Uni::Uni(string coursesPath, string studentsPath) {
+}
+
+/*void Uni::assignStudents() {
+    vector<Student>::iterator c;
+    for (s = this.students.begin(); s != this.studentsPath.end(); ++s) {
+    }
+}*/
