36bb240a4dd62f17409da697b4e96c401f1621bd
Ory Band <oryband@gmail.com>
2012-11-18 23:12:27 +0200

    Finsihed homework 3

--- spl/homework/homework_3/src/Run.cpp
+++ spl/homework/homework_3/src/Run.cpp
@@ -1,15 +1,22 @@
-
-#include "../include/LinkedList.h"
-
-
+#include "LinkedList.h"
 
 
 int main(int argc, char *argv[]) {
+    List list = List(),  // Link constructor.
+         list2,
+         list3;
 
-	return 0;
+    list2 = List(list);  // List copy-constructor.
+    list3 = list2;  // Operator =
 
+    list.insertData("first_link");  // Link constructor.
+
+    const Link *link = list.getHead();
+    Link *link2 = new Link(*link);  // Link copy-constructor.
+
+    delete link2;  // link destructor.
+
+    return 0;
 }
 
-
-
-
+// List destructor, more link destruction.
