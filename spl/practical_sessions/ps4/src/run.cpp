09e6b04fdb97da82a7dce779e75c9cb9fcb8db48
Ory Band <oryband@gmail.com>
2012-11-22 16:55:19 +0200

    Update gitignore to ignore C/C++ object files and such; Fixed pc4 opencv demo, it now compiles properly.

--- spl/practical_sessions/ps4/src/run.cpp
+++ spl/practical_sessions/ps4/src/run.cpp
@@ -1,19 +1,18 @@
 #include "ImageLoader.h"
 #include "ImageOperations.h"
- 
-int main(int argc, char **argv)
-{
-        ImageLoader img1("Lenna.png");
-        img1.displayImage();
- 
-        ImageOperations opr;
- 
-        ImageLoader img2(100,100);
-        opr.resize(img1.getImage(),img2.getImage());
-        img2.displayImage();
- 
-        ImageLoader img3(img1.getImage().size().height, img1.getImage().size().width * 2);
-        opr.copy_paste_image(img1.getImage(),img3.getImage(),0);
-        opr.copy_paste_image(img1.getImage(),img3.getImage(),img1.getImage().size().width);
-        img3.displayImage();
+
+int main(int argc, char **argv) {
+    ImageLoader img1("Lenna.png");
+    img1.displayImage();
+
+    ImageOperations opr;
+
+    ImageLoader img2(100,100);
+    opr.resize(img1.getImage(),img2.getImage());
+    img2.displayImage();
+
+    ImageLoader img3(img1.getImage().size().height, img1.getImage().size().width * 2);
+    opr.copyPasteImage(img1.getImage(),img3.getImage(),0);
+    opr.copyPasteImage(img1.getImage(),img3.getImage(),img1.getImage().size().width);
+    img3.displayImage();
 }
