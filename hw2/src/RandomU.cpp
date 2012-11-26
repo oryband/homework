8c4da8e0e8afd2e262cb4e6f65de02ef1d0f0b0b
Eldar Damari <eldar.damari@gmail.com>
2012-11-26 21:04:28 +0200

    RandomU (main function) finished.

--- hw2/src/RandomU.cpp
+++ hw2/src/RandomU.cpp
@@ -1,8 +1,6 @@
 #include <string>
-#include <vector>
 #include <iostream>
-
-#include <boost/algorithm/string.hpp>
+#include <algorithm>
 
 #include "Uni.h"
 
@@ -11,12 +9,30 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-    vector<string> words;
-    boost::split(words, argv[1], boost::is_any_of("="));  // FIXME Try '=' instead of "=".
 
-    // FIXME use lowercase() of some sort.
-    bool pgOn;
-    if (words[1].compare("yes") == 0) {
+    bool pgOn,
+         error = false;
+
+    string arg = "";
+
+    // Error tests.
+    if (argc != 2) {
+        error = true;
+    } else {
+        // Get malag= argument and convert to lower case.
+        arg = string(argv[1]);
+        transform(arg.begin(), arg.end(), arg.begin(), ::tolower);
+        if (arg.compare("malag=yes") != 0 && arg.compare("malag=no") != 0) {
+            error = true;
+        }
+    }
+
+    if (error) {
+        cout << "Bad program arguments. Use 'malag=yes' or 'malag=no' only." << endl;
+        exit(1);
+    }
+
+    if (arg == "malag=yes") {
         pgOn = true;
     } else {
         pgOn = false;
