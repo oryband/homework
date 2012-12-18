125fa78f942c7f1ca6da409aadf3bde4bff82be1
Ory Band <oryband@gmail.com>
2012-12-18 05:29:58 +0200

    Finished untested ScienceStoreTest file.

--- hw3/src/Company/ScienceStore/ScienceStore.java
+++ hw3/src/Company/ScienceStore/ScienceStore.java
@@ -5,19 +5,22 @@
  */
 package company;
 
+
 import java.util.HashMap; 
-//import java.util.ArrayList; 
 
 
 public class ScienceStore {
-    private HashMap equipmentPackages;
-    private HashMap scientists;
-    private HashMap laboratories;
+    private HashMap<String, EquipmentPackage> equipmentPackages;
+    private HashMap<String, Scientist> scientists;
+    private HashMap<String, Laboratory> laboratories;
 
-    public void purchaseEquipmentPackage() {}
-    public void AddNewEquipmentPackage(String k, Object o) {}
-    public void UpdateEquipmentPackage(String k, int d) {}  // d as in Difference - +1, -3, +10, etc.
+    public ScienceStore( 
+            HashMap<String, EquipmentPackage> equipmentPackages,
+            HashMap<String, Scientist> scientists,
+            HashMap<String, Laboratory> laboratories) {};
 
-    public void purchaseScientist() {}
-    public void purchaseLaboratory() {}
+    public void purchaseEquipmentPackage(String type, int amount) {}
+    public void purchaseScientist(String specializaiton, String name) {}
+    public void purchaseLaboratory(
+            String headOfLaboratory, String specialization) {}
 }
--- hw3/tests/TestScienceStore.java
+++ hw3/tests/TestScienceStore.java
@@ -12,31 +12,87 @@ public class TestScienceStore {
 
     private ScienceStore store;
 
-    @BeforeClass
-    public static void oneTimeSetup() {
-        return;
-    }
+    //@BeforeClass
+    //public static void oneTimeSetup() {}
 
-    @AfterClass
-    public static void oneTimeTearDown() {
-        return;
-    }
+    //@AfterClass
+    //public static void oneTimeTearDown() {}
 
     @Before
     public static void setup() {
-        ScienceStore store = new ScienceStore();
+        // TODO Create hashmaps for constructor.
+        ScienceStore store = new ScienceStore(/*TODO add them here*/);
     }
 
-    @After
-    public static void teardown() {
-        return;
-    }
+    //@After
+    //public static void teardown() {}
 
-    // test case method name should start with test.
     @Test
-    public final void testAdd() {
-        assertEquals(calcEngine.add(20, 30), 50);
+    public final void testPurchaseEquipmentPackage() {
+        store.PurchaseEquipmentPackage("Microscope", 10);
     }
+
+    @Test(expected= RuntimeException.class)
+    public final void testMissingTypePurchaseEquipmentPackage() {
+        store.purchaseEquipmentPackage("MISSING", 10);
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testMissingAmountPurchaseEquipmentPackage() {
+        store.purchaseEquipmentPackage("Microscope", 5);
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testPurchaseTooManyEquipmentPackage() {
+        store.purchaseEquipmentPackage("Microscope", 10, 100);
+        store.purchaseEquipmentPackage("Microscope", 10, 100);
+        store.purchaseEquipmentPackage("Microscope", 10, 100);
+    }
+
+
+    @Test
+    public final void testPurchaseScientist() {
+        store.PurchaseScientist("Family", "Steve");
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testMissingSpecializationPurchaseScientist() {
+        store.PurchaseScientist("MISSING", "Steve");
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testMissingNamePurchaseScientist() {
+        store.PurchaseScientist("Children", "NO_NAME");
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testPurchaseTooManyScientist() {
+        store.purchaseScientist("Family", "Steve");
+        store.purchaseScientist("Family", "Steve");
+    }
+
+
+    @Test
+    public final void testPurchaseLaboratory() {
+        store.PurchaseLaboratory("House", "Family");
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testMissingHeadOfLaboratoryPurchaseLaboratory() {
+        store.PurchaseLaboratory("NO_NAME", "Family");
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testMissingSpecializationPurchaseLaboratory() {
+        store.PurchaseLaboratory("House", "MISSING");
+    }
+
+    @Test(expected= RuntimeException.class)
+    public final void testPurchaseTooManyLaboratories() {
+        store.PurchaseLaboratory("House", "Family");
+        store.PurchaseLaboratory("House", "Family");
+    }
+
 
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(MyClassTest.class);
