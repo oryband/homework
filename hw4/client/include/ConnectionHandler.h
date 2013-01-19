6464a698f7a7babdd2651d3a29d363202b9c013e
Ory Band <oryband@gmail.com>
2013-01-19 19:37:18 +0200

    Client ConnectionHandler finished, untested.

--- hw4/client/include/ConnectionHandler.h
+++ hw4/client/include/ConnectionHandler.h
@@ -4,14 +4,53 @@
 
 #include <string>
 
+#include <boost/asio.hpp>
+
 
 class ConnectionHandler {
     private:
         const std::string _host;
         const short _port;
 
+        boost::asio::io_service _io;
+        boost::asio::ip::tcp::socket _socket;
+
     public:
         ConnectionHandler(const std::string host, const short port);
+        virtual ~ConnectionHandler();
+
+        const bool connect();
+        void close();
+
+        /* Read a fixed number of bytes from the server - blocking.
+         * Returns false in case the connection is closed before bytesToRead bytes can be read.
+         */
+        const bool getBytes(char bytes[], const unsigned int size);
+
+        /* Send a fixed number of bytes from the client - blocking.
+         * Returns false in case the connection is closed before all the data is sent.
+         */
+        const bool sendBytes(const char bytes[], const unsigned int size);
+
+        /* Read an ascii line from the server
+         * Returns false in case connection closed before a newline can be read.
+         */
+        const bool getLine(std::string& line);
+
+        /* Send an ascii line from the server
+         * Returns false in case connection closed before all the data is sent.
+         */
+        const bool sendLine(const std::string& line);
+
+        /* Get Ascii data from the server until the delimiter character
+         * Returns false in case connection closed before null can be read.
+         */
+        const bool getFrameAscii(std::string& frame, const char delimter);
+
+        /* Send a message to the remote host.
+         * Returns false in case connection is closed before all the data is sent.
+         */
+        const bool sendFrameAscii(const std::string& frame, const char delimter);
 };
 
 
--- hw4/client/src/ConnectionHandler.cpp
+++ hw4/client/src/ConnectionHandler.cpp
@@ -1,9 +1,142 @@
 #include "ConnectionHandler.h"
 
 
-using namespace std;
+using std::string;
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::exception;
+
+using boost::asio::ip::tcp;
+using boost::asio::ip::address;
+using boost::asio::buffer;
+using boost::system::error_code;
+using boost::system::system_error;
 
 
-ConnectionHandler :: ConnectionHandler(const string host, const short port) :
-    _host(host), _port(port) {}
+ConnectionHandler :: ConnectionHandler(
+        const string host, const short port) :
+    _host(host), _port(port), _io(), _socket(_io) {}
 
+
+ConnectionHandler :: ~ConnectionHandler() {
+    close();
+}
+
+
+const bool ConnectionHandler :: connect() {
+    cout << "Connecting to " << _host << ":" << _port << endl;
+
+    try {
+        // Server's end-point.
+        tcp::endpoint endpoint(address::from_string(_host), _port);
+        error_code error;
+
+        _socket.connect(endpoint, error);
+
+        if (error) {
+            throw system_error(error);
+        }
+    } catch (exception& e) {
+        cerr << "connect() failed (Error: " << e.what() << ")" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+void ConnectionHandler :: close() {
+    try {
+        _socket.close();
+    } catch (exception& e) {
+        cout << "close() failed: connection already closed" << endl;
+    }
+}
+
+const bool ConnectionHandler :: getBytes(
+        char bytes[], const unsigned int size) {
+
+    size_t tmp = 0;
+    error_code error;
+
+    try {
+        while ( ! error && size > tmp ) {
+            tmp += _socket.read_some(
+                    buffer(bytes + tmp, size - tmp),
+                    error);
+        }
+
+        if (error) {
+            throw system_error(error);
+        }
+    } catch (exception e) {
+        cerr << "getBytes() failed (Error: " << e.what() << ")" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+const bool ConnectionHandler :: sendBytes(
+        const char bytes[], const unsigned int size) {
+    size_t tmp = 0;  // FIXME should be int?
+    error_code error;
+
+    try {
+        while ( ! error && size > tmp ) {
+            tmp += _socket.write_some(
+                    buffer(bytes + tmp, size - tmp),
+                    error);
+        }
+		if (error) {
+			throw system_error(error);
+        }
+    } catch (exception& e) {
+        cerr << "sendBytes() failed (Error: " << e.what() << ")" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+const bool ConnectionHandler :: getLine(string& line) {
+    return getFrameAscii(line, '\n');
+}
+
+
+const bool ConnectionHandler :: sendLine(const string& line) {
+    return sendFrameAscii(line, '\n');
+}
+
+
+const bool ConnectionHandler :: getFrameAscii(
+        string& frame, const char delimeter) {
+    char ch;
+
+    try {
+        do {
+            getBytes(&ch, 1);
+            frame.append(1, ch);
+        } while (delimeter != ch);
+    } catch (exception& e) {
+        cerr << "getFrameAscii() failed (Error: " << e.what() << ")" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+const bool ConnectionHandler :: sendFrameAscii(
+        const string& frame, const char delimiter) {
+
+	if ( ! sendBytes(frame.c_str(), frame.length())) {
+        return false;
+    } else {
+        return sendBytes(&delimiter, 1);
+    }
+}
