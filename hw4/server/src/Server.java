016bd604f7ab191decc7485e0b74db93186ebdd4
Ory Band <oryband@gmail.com>
2013-01-23 20:04:10 +0200

    Changed encoding from us-ascii back to utf-8

--- hw4/server/src/Server.java
+++ hw4/server/src/Server.java
@@ -3,7 +3,7 @@ import irc.ThreadPerClientServer;
 
 public class Server {
     public static void main(String[] args) {
-        ThreadPerClientServer server = new ThreadPerClientServer(6667, "US-ASCII");
+        ThreadPerClientServer server = new ThreadPerClientServer(6667, "UTF-8");
         Thread serverThread = new Thread(server);
         serverThread.start();
 
