934a3ad684e66ec18f151a26f56621f5ad59d6dd
Ory Band <oryband@gmail.com>
2013-03-14 13:42:21 +0200

    Almost finished task-1-b

--- task-1/src/cycle_split.c
+++ task-1/src/cycle_split.c
@@ -1,19 +1,50 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main(int argc, char **argv) {
     FILE *out=stdout,
          *in=stdin;
     char c;
+    char *d = "W";
+    int num, counter;
 
     puts("---");
 
-    while ((c = fgetc(in)) != EOF) {
-        if (isspace(c)) {
+    if (argc == 3 && strcmp(argv[1], "-d") == 0) {
+        d = argv[2];
+    }
+
+    if (strcmp(d, "W") == 0) {
+        while ((c = fgetc(in)) != EOF) {
+            if (isspace(c)) {
+                fputc('\n', out);
+            } else {
+                fputc(c, out);
+            }
+        }
+    } else if (strcmp(d, "C") == 0) {
+        while ((c = fgetc(in)) != EOF) {
+            if (c == ',') {
+                fputc('\n', out);
+            } else {
+                fputc(c, out);
+            }
+        }
+    } else {
+        num = atoi(d);
+        counter = 0;
+
+        while ((c = fgetc(in)) != EOF) {
+            while (++counter % num != 0) { 
+                fputc(c, out);
+            }
+
             fputc('\n', out);
-        } else {
-            fputc(c, out);
+
+            counter = 0;
         }
     }
 
