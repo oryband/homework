b4e1d709545efaa92348284842fbd01f59f01457
Ory Band <oryband@gmail.com>
2013-03-15 19:29:52 +0200

    Finished task 1

--- task-1/src/cycle_split.c
+++ task-1/src/cycle_split.c
@@ -4,18 +4,58 @@
 #include <ctype.h>
 
 
+void W(FILE *in, FILE *out, const char *d) {
+    char c;
+    while ((c = fgetc(in)) != EOF) {
+        if (isspace(c)) {
+            fputc('\n', out);
+        } else {
+            fputc(c, out);
+        }
+    }
+}
+
+
+void C(FILE *in, FILE *out, const char *d) {
+    char c;
+    while ((c = fgetc(in)) != EOF) {
+        if (c == ',') {
+            fputc('\n', out);
+        } else {
+            fputc(c, out);
+        }
+    }
+}
+
+
+void D(FILE *in, FILE *out, const char *d) {
+    char c;
+    int num = atoi(d),
+        counter = 0;
+
+    while (c != EOF) { 
+        while ((c = fgetc(in)) != EOF && ++counter < num) {
+            fputc(c, out);
+        }
+
+        fputc('\n', out);
+
+        counter = 0;
+    }
+}
+
+
 int main(int argc, char **argv) {
     FILE *out=stdout,
          *in=stdin;
-    char c=0;
     char *d = "W";
-    int i, num, counter;
+    int i;
 
     puts("---");
 
     for (i=1; i < argc; i++) {
         if (strcmp(argv[i],"-i") == 0) {
-            out = fopen(argv[++i], "w");
+            in = fopen(argv[++i], "r");
         } else if (strcmp(argv[i],"-d") == 0) {
             d = argv[++i];
         } else {
@@ -25,40 +65,21 @@ int main(int argc, char **argv) {
     }
 
     if (strcmp(d, "W") == 0) {
-        while ((c = fgetc(in)) != EOF) {
-            if (isspace(c)) {
-                fputc('\n', out);
-            } else {
-                fputc(c, out);
-            }
-        }
+        W(in, out, d);
     } else if (strcmp(d, "C") == 0) {
-        while ((c = fgetc(in)) != EOF) {
-            if (c == ',') {
-                fputc('\n', out);
-            } else {
-                fputc(c, out);
-            }
-        }
+        C(in, out, d);
     } else {
-        num = atoi(d);
-        counter = 0;
-
-        while (c != EOF) { 
-            while ((c = fgetc(in)) != EOF && ++counter < num) {
-                fputc(c, out);
-            }
-
-            fputc('\n', out);
-
-            counter = 0;
-        }
+        D(in, out, d);
     }
 
-    fputs("^D", out);
+    /*fputs("^D", out);*/
 
     if (out != stdout) {
         fclose(out);
+    }
+
+    if (in != stdin) {
+        fclose(in);
     }
 
     return 0;
