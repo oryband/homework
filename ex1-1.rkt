4dd7469bf1cf8b28b2d0f83287dd434095e2ee59
Ory Band <oryband@gmail.com>
2013-03-29 21:14:33 +0300

    Added initial ex1-1 code

---
+++ ex1-1.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+;--------------;
+;  question 3  ;
+;--------------;
+; Signature: deep-sum-digit(n)
+; Type: 
+; Purpose: 
+; Pre-conditions: 
+; Tests: 
+ (define deep-sum-digit 
+   (lambda (n)
+     (...)))
+ 
+ 
+;--------------;
+;  question 4  ;
+;--------------;
+; Signature: 
+; Type: 
+; Purpose: Approximates 1/e as the sum of n first elements from n_i=(-1)^i/i!
+; Pre-conditions: n>=0, integer 
+; Tests: 
+ (define calc-1-e
+   (lambda (n)
+     (...)))
+ 
+;You can add functions if needed, 
+;remeber to add a contract
+;Note the order of the contract!!!
+ 
+ 
+ 
