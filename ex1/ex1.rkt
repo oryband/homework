82b0ba56acab66b220d09e098e02aa7ae1266f6c
Ory Band <oryband@gmail.com>
2013-03-31 14:53:42 +0300

    question 4 finished.

--- ex1/ex1.rkt
+++ ex1/ex1.rkt
@@ -1,33 +1,36 @@
+#! /usr/bin/env racket
+
 #lang racket
+
 
 ;--------------;
 ;  question 3  ;
 ;--------------;
 ; Signature: deep-sum-digit(n)
-; Type: 
-; Purpose: 
-; Pre-conditions: 
-; Tests: 
- (define deep-sum-digit 
-   (lambda (n)
-     (...)))
- 
- 
+; Type: [Number -> Number]
+; Purpose: calculates the sum of digits of the sum of digits up to a number smaller than 10.
+; Pre-conditions: n>=0, integer.
+; Tests: (test (deep-sum-digit 4526) => 8)
+(define (deep-sum-digit n) (sum-digit (sum-digit n 0) 0))
+
+; Signature: sum-digit(n acc)
+; Type: [Number, Number -> Number]
+; Purpose: sum-digit iterator, tail recursive.
+; Pre-conditions: n,acc>=0, integers.
+(define (sum-digit n acc)
+  (if (= n 0)
+    acc
+    (sum-digit (quotient n 10) (+ acc (remainder n 10)))))
+
+
 ;--------------;
 ;  question 4  ;
 ;--------------;
-; Signature: 
-; Type: 
+; Signature:
+; Type:
 ; Purpose: Approximates 1/e as the sum of n first elements from n_i=(-1)^i/i!
-; Pre-conditions: n>=0, integer 
-; Tests: 
- (define calc-1-e
-   (lambda (n)
-     (...)))
- 
-;You can add functions if needed, 
-;remeber to add a contract
-;Note the order of the contract!!!
- 
- 
- 
+; Pre-conditions: n>=0, integer
+; Tests:
+(define calc-1-e
+  (lambda (n)
+    (...)))
