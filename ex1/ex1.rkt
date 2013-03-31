34b91d943cb18520562f1dd8473dd46346601790
Ory Band <oryband@gmail.com>
2013-03-31 15:41:59 +0300

    Finished question 5, ex1 is finished.

--- ex1/ex1.rkt
+++ ex1/ex1.rkt
@@ -6,31 +6,41 @@
 ;--------------;
 ;  question 3  ;
 ;--------------;
-; Signature: deep-sum-digit(n)
+; Signature: deep-sum-digit (n)
 ; Type: [Number -> Number]
 ; Purpose: calculates the sum of digits of the sum of digits up to a number smaller than 10.
 ; Pre-conditions: n>=0, integer.
 ; Tests: (test (deep-sum-digit 4526) => 8)
-(define (deep-sum-digit n) (sum-digit (sum-digit n 0) 0))
+(define (deep-sum-digit n)
+  (define (sum-digit n acc)
+    (if (= n 0)
+      acc
+      (sum-digit (quotient n 10) (+ acc (remainder n 10)))))
 
-; Signature: sum-digit(n acc)
-; Type: [Number, Number -> Number]
-; Purpose: sum-digit iterator, tail recursive.
-; Pre-conditions: n,acc>=0, integers.
-(define (sum-digit n acc)
-  (if (= n 0)
-    acc
-    (sum-digit (quotient n 10) (+ acc (remainder n 10)))))
+  (sum-digit (sum-digit n 0) 0))
 
 
 ;--------------;
 ;  question 4  ;
 ;--------------;
-; Signature:
-; Type:
+; Signature: calc-1-e (n)
+; Type: [Number -> Number]
 ; Purpose: Approximates 1/e as the sum of n first elements from n_i=(-1)^i/i!
 ; Pre-conditions: n>=0, integer
-; Tests:
-(define calc-1-e
-  (lambda (n)
-    (...)))
+; Tests: (test (calc-1-e 6) => 103/280)
+(define (calc-1-e n)
+  (define (factorial n)
+    (define (factorial-iter n acc)
+      (if (= n 0)
+        acc
+        (factorial-iter (sub1 n) (* n acc))))
+
+    (factorial-iter n 1))
+
+  (define (calc-1-e-iter n acc)
+    (if (= n 0)
+      acc
+      (calc-1-e-iter (sub1 n)
+                     ((if (even? n) + -) acc (/ 1 (factorial n))))))
+
+  (calc-1-e-iter (add1 n) 1))
