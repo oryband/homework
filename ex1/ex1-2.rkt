5fe2bf30e562244a76f758f5736602d16e68ed2c
Ory Band <oryband@gmail.com>
2013-03-31 16:21:03 +0300

    added ex1-2.rkt template file

---
+++ ex1/ex1-2.rkt
@@ -0,0 +1,107 @@
+#lang racket
+
+;----------------;
+;  Question 1.a  ;
+;----------------;
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define ln2-rec 
+  (lambda(n)
+    (...)))
+
+
+;----------------;
+;  Question 1.b  ;
+;----------------;
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define third-iter
+  (lambda(n acc)
+    (...)))
+
+;----------------;
+;  Question 1.c  ;
+;----------------;
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define sum-alt
+  (lambda(term a next b)
+    (...)))
+
+;----------------;
+;  Question 1.d  ;
+;----------------;
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define term-ln2
+  (lambda(n)
+    (...)))
+
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define next-ln2
+  (lambda(n)
+    (...)))
+
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define term-third
+  (lambda(n)
+    (...)))
+
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define next-third
+  (lambda(n)
+    (...)))
+
+;----------------;
+;   Question 2   ;
+;----------------;
+; Signature:
+; Type: 
+; Purpose: 
+; Example: 
+; Pre-conditions:
+; Post-conditions:
+; Tests: 
+(define con-func
+  (lambda(f a flag)
+    (...)))
+
+ 
