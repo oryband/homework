28f9e810ce9e5a2b364d5d5ce097feba51e40fb5
Ory Band <oryband@gmail.com>
2013-04-07 14:11:34 +0300

    Prettified count-words.c

--- task-2/src/count-words.c
+++ task-2/src/count-words.c
@@ -3,27 +3,28 @@
 #include <stdio.h>
 #include <string.h>
 
+
 /* return string "word" if the count is 1 or "words" otherwise */
 char *words(int count) {
-  char *words = "words";
-  if(count==1) 
-    words[strlen(words)-1] = '\0';
-  return words;
+    char *words = "words";
+    if(count == 1)
+        words[strlen(words)-1] = '\0';
+    return words;
 }
+
 
 /* print a message reportint the number of words */
 int print_word_count(char **argv) {
-  int count = 0;
-  char **a = argv;
-  while(*(a++))
-    ++count;
-  printf("The sentence contains %d %s.\n", count, words(count));
-  return count;
+    int count = 0;
+    char **a = argv;
+    while(*(a++))
+        ++count;
+    printf("The sentence contains %d %s.\n", count, words(count));
+    return count;
 }
+
 
 /* print the number of words in the command line and return the number as the exit code */
 int main(int argc, char **argv) {
-  return print_word_count(argv+1);
+    return print_word_count(argv+1);
 }
-
-   
