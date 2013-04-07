9c25264a5cd3f04c25b85eb92e94b04efcbdffeb
Ory Band <oryband@gmail.com>
2013-04-07 17:17:44 +0300

    added initial oop.c

---
+++ task-2/src/oop.c
@@ -0,0 +1,29 @@
+28 lines ...#include <stdio.h>
+ 
+/* forward declarations: */
+void rec_map(int*, int, int (*f) (int));
+ 
+struct int_array {
+  int *array;
+  int sz;
+  void (*map) (struct int_array *, int (*f) (int));
+};
+ 
+void int_array_map(struct int_array *iarray, int (*f) (int)){
+  /* TODO: complete during task 2.c */
+}
+ 
+void initialize_int_array(struct int_array *iarray, int *array, int sz){
+  /* TODO: complete during task 2.c */
+}
+ 
+/* TODO: 2.a functions here */
+ 
+ 
+void rec_map(int *array, int sz, int (*f) (int)){
+  /* TODO: Complete during task 2.b */
+}
+ 
+int main(int argc, char **argv){
+  /* TODO: Test your code */
+}
