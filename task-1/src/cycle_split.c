b86db78817655565cb5422487d57346b3be7c642
Ory Band <oryband@gmail.com>
2013-04-14 17:32:34 +0300

    WOI for task-1, last sub-task (2)

--- task-1/src/cycle_split.c
+++ task-1/src/cycle_split.c
@@ -4,11 +4,29 @@
 #include <ctype.h>
 
 
-void W(FILE *in, FILE *out, const char *d) {
-    char c;
+FILE *nextFile(FILE *outs[], int i) {
+    int len = sizeof(outs) / sizeof(outs[0]);
+
+    if (i > len -1) {
+        i = 0;
+    }
+    while (outs[i] == 0) {
+        i++;
+        if (i == len -1) {
+            i = 0;
+        }
+    }
+
+    return outs[i];
+}
+
+void W(FILE *in, FILE *outs[], const char *d) {
+    char c, i=0;
+    FILE *out = outs[0];
     while ((c = fgetc(in)) != EOF) {
         if (isspace(c)) {
             fputc('\n', out);
+            out = nextFile(outs, ++i);
         } else {
             fputc(c, out);
         }
@@ -16,40 +34,44 @@ void W(FILE *in, FILE *out, const char *d) {
 }
 
 
-void C(FILE *in, FILE *out, const char *d) {
+/*void C(FILE *in, FILE *outs, const char *d) {
     char c;
     while ((c = fgetc(in)) != EOF) {
         if (c == ',') {
-            fputc('\n', out);
+            fputc('\n', outs);
         } else {
-            fputc(c, out);
+            fputc(c, outs);
         }
     }
-}
+}*/
 
 
-void D(FILE *in, FILE *out, const char *d) {
+/*void D(FILE *in, FILE *outs, const char *d) {
     char c;
     int num = atoi(d),
         counter = 0;
 
     while (c != EOF) { 
         while ((c = fgetc(in)) != EOF && ++counter < num) {
-            fputc(c, out);
+            fputc(c, outs);
         }
 
-        fputc('\n', out);
+        fputc('\n', outs);
 
         counter = 0;
     }
-}
+}*/
 
 
 int main(int argc, char **argv) {
-    FILE *out=stdout,
-         *in=stdin;
+    FILE *outs[3]={ stdout, 0, 0 };
+    FILE *out;
+    FILE *in = stdin;
+
     char *d = "W";
-    int i;
+
+    int i, j=0, len;
+
 
     puts("---");
 
@@ -59,23 +81,36 @@ int main(int argc, char **argv) {
         } else if (strcmp(argv[i],"-d") == 0) {
             d = argv[++i];
         } else {
-            printf("invalid parameter - %s\n", argv[i]);
-            return 1;
+            if (j > 3) {
+                printf("Output files should be <= 3");
+                return -1;
+            }
+
+            if (strcmp("stdout", argv[i]) != 0) {
+                outs[j++] = fopen(argv[i], "w");
+            } else {
+                outs[j++] = stdout;
+            }
         }
     }
 
     if (strcmp(d, "W") == 0) {
-        W(in, out, d);
+        W(in, outs, d);
     } else if (strcmp(d, "C") == 0) {
-        C(in, out, d);
+        0;
+        /*C(in, outs, d);*/
     } else {
-        D(in, out, d);
+        0;
+        /*D(in, outs, d);*/
     }
 
-    /*fputs("^D", out);*/
+    /*fputs("^D", outs);*/
 
-    if (out != stdout) {
-        fclose(out);
+    len = sizeof(outs) / sizeof(outs[0]);
+    for (i=0; i < len; i++) {
+        if (out != stdout && out != 0) {
+            fclose(out);
+        }
     }
 
     if (in != stdin) {
