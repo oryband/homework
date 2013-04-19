4844e4758eaf66ede9f85bbe3321fa8b448eb142
Ory Band <oryband@gmail.com>
2013-04-19 19:04:59 +0300

    Added some cosmetic prints

--- ex2/questions.rkt
+++ ex2/questions.rkt
@@ -3,6 +3,7 @@
 #lang racket
 ;#lang lazy
 
+
 ; 1.a
 (define foo
   (lambda (x) (display x) (newline) (+ x 2)))
@@ -10,21 +11,25 @@
 (define goo
   (lambda (x) (display 5) (newline) (+ x 1)))
 
+(display "1:\n")
 (foo (goo 0))
 
 
 ; 2.a.1
+(display "\n2.a.1:\n")
 (lambda (f n)
   (lambda (x)
     (* n (f x))))
 
 ; 2.a.2
+(display "\n2.a.2:\n")
 (lambda (f n)
   (lambda (x)
     (* n (f x)))
   f 3)
 
 ; 2.a.3
+(display "\n2.a.3:\n")
 (define dg
   (lambda (number)
     (if (< number 10)
