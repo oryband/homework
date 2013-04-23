f73598111347e988bc998bebb66e782c23c93b15
Ory Band <oryband@gmail.com>
2013-04-23 19:22:08 +0300

    updated ex2.rkt to latest version

--- ex2/ex2.rkt
+++ ex2/ex2.rkt
@@ -1,25 +1,35 @@
 #lang racket
 (provide (all-defined-out))
-
-(define flatmap
-  (lambda(f seq)
-    (if (null? seq)
-        '()
-        (append (f (car seq))
-                (flatmap f (cdr seq))))))
-
+;Signature: fresh-name()
+;Type: [Empty->Symbol]
+;Purpose: generating new names
+;Tests: (fresh-name)=>'var_0, (fresh-name) => 'var_1
+;       Or(fresh-name 'T) => (fresh-name 'T) => 'T_0, (fresh-name 'T) => 'T_1
 (define fresh-name
-  (let((counter 0))
+  ((lambda(counter)
     (lambda name
       (if(null? name)
          (fresh-name 'var)
-         (let((new-name (string->symbol (string-append (symbol->string (car name)) ":" (number->string counter)))))
+         (let((new-name (string->symbol (string-append (symbol->string (car name)) "_" (number->string counter)))))
            (set! counter (+ 1 counter))
-           new-name)))))
+           new-name)))) 0))
 
 
-(define (tagged-list? exp tag)
-  (and (list? exp) (eq? (car exp) tag)))
+;Signature: get-tag(x)
+;Type: [Tagged-data(T) -> Symbol]
+;Purpose: Select the tag from a tagged-data value
+;Tests: (get-tag '(lambda(x) (+ x 1))) => 'lambda
+(define get-tag (lambda (x) (car x)))
+
+
+;Signature: tagged-list?(exp, tag)
+;Type: [Tagged-data(T)*Symbol -> Boolean]
+;Purpose: Identify tagged-data values
+;Tests: (tagged-list? '(lambda(x) x) 'lambda) => #t
+(define tagged-list?
+  (lambda (exp tag)
+    (and (list? exp)
+         (eq? (get-tag exp) tag))))
 
 
 
@@ -27,45 +37,76 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Variable
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (variable? exp) (symbol? exp))
+;Signature: variable?(exp)
+;Type: [Tagged-data(T)->Boolean]
+;Purpose: Identify variables
+;Tests: (variable? 'x) => #t
+(define variable? (lambda(exp) (symbol? exp)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Atomic
+; Atomic data
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (atomic? exp)
-  (or (number? exp) (boolean? exp) (variable? exp) (null? exp)))
+;Signature: atomic?(exp)
+;Type: [Tagged-data(T)->Boolean]
+;Purpose: identifying atomic expressions
+;Tests: (atomic? '(+ x 1)) => #f, (atomic? 'x) => #t
+(define atomic?
+  (lambda (exp)
+    (or (number? exp) (boolean? exp) (variable? exp) (null? exp))))
 
 
+; Signature: get-content(exp)
+; Type: [LIST -> T]
+;Purpose: Select the data from a tagged-data value
+;Tests: (get-content '(lambda(x y) (+ x 1) (+ y 2))) => '((+ x 1) (+ y 2))
+(define get-content
+  (lambda (exp) (cdr exp)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Quoted
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;Signature: quoted?(exp)
+;Type:[[Tagged-data(T)->Boolean]
+;Purpose: identifying quoted expressions
+;Tests: (quoted? '(quote x)) => #t
+(define quoted?
+  (lambda (exp)
+    (tagged-list? exp 'quote)))
 
-(define (quoted? exp)
-  (tagged-list? exp 'quote))
-
-
-(define (text-of-quotation exp) (cadr exp))
+;Signature: text-of-quotation(exp)
+;Type: [Tagged-data(T) => Symbol]
+;Purpose: Select the data from a tagged-data value
+;Tests: (text-of-quotation '(quote x)) => 'x
+(define text-of-quotation
+  (lambda (exp) (car (get-content exp))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Lambda
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (lambda? exp) (tagged-list? exp 'lambda))
+;Signature: lambda?(exp)
+;Type: [Tagged-data(T) => Boolean]
+;Purpose: identifying lambda expressions
+;Tests:(lambda? '(lambda() 1)) => #t
+(define lambda?
+  (lambda (exp)
+    (tagged-list? exp 'lambda)))
 
 
-(define (lambda-parameters exp) (cadr exp))
-(define (lambda-body exp) (cddr exp))
+;Signature: lambda-body(exp)
+;Type: [Tagged-data(T) => List[Symbol]
+;Purpose: Select the lambda body from a lambda expression
+;Tests: (lambda-body '(lambda(x y) (+ x 1) (+ y 2))) => '((+ x 1) (+ y 2))
+(define lambda-body
+  (lambda (exp)
+    (cdr (get-content exp))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Your Code
-;;;Signature: generate-type-vars (exp)
-;;;Purpose: returns the sequence of sub-expressions with their type variables
-;;;Type: T -> List[List[Symbol]
-;;;Examples:
-;;;(generate-type-vars '(lambda(x) x))=> '(((lambda (x) x) var_0) (x var_1))
-;;;(generate-type-vars 'x)=> '((x var_0))
-;;;(generate-type-vars 1)=>'((1 var_0))
+
+;Signature:generate-type-vars (exp)
+;Type: T -> List[List[Symbol]
+;Purpose:returns the sequence of sub-expressions with their type variables
+;Tests:(generate-type-vars '(lambda(x) x))=> '(((lambda (x) x) var_0) (x var_1))
