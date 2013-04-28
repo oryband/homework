a02c130916c04ceee5536b332de82829a83139d8
Ory Band <oryband@gmail.com>
2013-04-28 16:34:27 +0300

    Added hello-world example file

---
+++ lab-4/src/hello-world.c
@@ -0,0 +1,12 @@
+#include "util.h"
+
+#define SYS_WRITE 4
+#define STDOUT 1
+
+
+int main (int argc , char* argv[], char* envp[]) {
+    char * str = "hello world\n";
+    system_call(SYS_WRITE, STDOUT, str, strlen(str));
+    return 0;
+}
+
