b2f902383e831b4f4f0bdd34a7ecef99f23f1593
Ory Band <oryband@gmail.com>
2013-06-02 17:50:51 +0300

    finished tasks 1a, 1b

--- lab-7/src/hexeditplus.c
+++ lab-7/src/hexeditplus.c
@@ -2,23 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ERROR "Error"
 
-void fun(char *f) {
+
+void quit(char *f) {
     exit(EXIT_SUCCESS);
 }
 
 
-int main(int argc, char **argv) {
-    char *labels[]                = { "Mem Display" , "File Display" , "Quit" };
-    void (*functions[]) (char *f) = { fun           , fun            , fun };
+void memdisplay(char *filename) {
+    int address, length,
+        i;
+
+    printf("<address> <length>: ");
+    scanf("%x %d", &address, &length);
+    for (i=0; i<length; i++) {
+        printf("%02X ", ((unsigned char *) address)[i]);
+    }
+    printf("\n");
+}
+
+
+void filedisplay(char *filename) {
+    int address, length,
+        i;
+    FILE *f = fopen(filename, "r");
+
+    if (f == NULL) {
+        perror(ERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("<address> <length>: ");
+    scanf("%x %d", &address, &length);
+
+    if (fseek(f, address, SEEK_SET) == -1) {
+        perror(ERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    for (i=0; i<length; i++) {
+        printf("%02X ", (unsigned char *) (fgetc(f)));
+    }
+    printf("\n");
+    fclose(f);
+}
+
+
+int main(int argc, char *argv[]) {
+    char *labels[]                       = { "Mem Display" , "File Display" , "Quit" };
+    void (*functions[]) (char *filename) = { memdisplay    , filedisplay    , quit };
     int i=0,
         choice;
 
     for (i=0; i < sizeof(labels) / sizeof(labels[0]); i++) {
-        printf("[%d]\t%s\n", i, labels[i]);
+        printf("[%d]\t%s\n", i+1, labels[i]);
     }
     scanf("%d", &choice);
-    functions[choice](NULL);
+    functions[choice-1](argv[1]);
 
     return EXIT_SUCCESS;
 }
