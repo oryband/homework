fd618525b527287fdab662b01bff481c3dccc1be
Ory Band <oryband@gmail.com>
2013-06-02 18:51:49 +0300

    WOI task4

---
+++ lab-7/src/is_palindrome.c
@@ -0,0 +1,15 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 1024
+#define ERROR "Error"
+
+
+int main(int argc, char *argv[]) {
+    int i;
+
+    /* TODO: Finish task 4. */
+    /* for (i=0; i  */
+    return EXIT_SUCCESS;
+}
