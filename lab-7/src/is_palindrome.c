35aa08b5e1bf2c253a4bf3453377d4813b30a4ff
Ory Band <oryband@gmail.com>
2013-06-23 17:50:09 +0300

    finished lab 7 task 4

--- lab-7/src/is_palindrome.c
+++ lab-7/src/is_palindrome.c
@@ -1,24 +1,28 @@
-#include <stdio.h>
-#include <string.h>
+/* Cannot use includes, external functions. */
+#include <stdio.h>  /* Not used in is_pal(). */
 
+#define NULL 0
 #define false 0
-#define true 0
+#define true 1
+#define EXIT_SUCCESS 0
 
+/* Cannot exceet ~300 bytes. */
 int is_pal(char *w) {
-    int i,
-        l = strlen(w);
+    int i, l;
+
+    for (l=0; w[l] != NULL; l++);  /* Calculate length. */
 
     for (i=0; i<l/2; i++) {
         if (w[i] != w[l-1-i]) {
-            printf("%s is not a palindrome!\n", w);
             return false;
         }
     }
 
-    printf("%s is a palindrome!\n", w);
     return true;
 }
 
 int main(int argc, char *argv[]) {
-    return is_pal(argv[1]);
+    printf("%s is %sa palindrome!\n", argv[1], is_pal(argv[1]) ? "" : "not ");
+    return EXIT_SUCCESS;
 }
+
