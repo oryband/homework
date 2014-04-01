56babbc7109e8b3d142d47e55df7f8c012b3cf6a
Dvir Azulay <dvir.azulay@gmail.com>
2014-04-01 19:21:02 +0300

    Fixed a few lint issues

--- Exercise 2/part2/dvirazulay_rps.js
+++ Exercise 2/part2/dvirazulay_rps.js
@@ -1,11 +1,11 @@
 var options = ['paper', 'rock', 'scissors'];
 
 function is_valid_rps(item) {
-  return options.indexOf(item) != -1;
+  return options.indexOf(item) !== -1;
 }
 
 function random_rps() {
-  return options[Math.floor(Math.random()*3)];
+  return options[Math.floor(Math.random() * 3)];
 }
 
 function rps(item1, item2) {
@@ -33,14 +33,14 @@ function rps(item1, item2) {
   document.getElementById('player2').innerHTML = item2;
 
   // if both players chose the same item, we have a tie
-  if (item1 == item2) {
+  if (item1 === item2) {
     document.getElementById('winner').innerHTML = 'There is a tie :(';
     return;
   }
 
-  if ((item1 == 'rock' && item2 == 'scissors')
-      || (item1 == 'paper' && item2 == 'rock')
-      || (item1 == 'scissors' && item2 == 'paper')) {
+  if ((item1 === 'rock' && item2 === 'scissors')
+      || (item1 === 'paper' && item2 === 'rock')
+      || (item1 === 'scissors' && item2 === 'paper')) {
     // player1 won. in any other case - player2 won. (we already checked for ties)
     document.getElementById('winner').innerHTML = 'Player 1 is victorious!';
     return;
