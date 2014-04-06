cd52d706948c53ea43e8371e966ed433e2af9fb5
Ory Band <oryband@gmail.com>
2014-04-06 23:11:19 +0300

    updated ass-1 scripts for text testing.

--- ass-1/run.sh
+++ ass-1/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf out && mvn package && hadoop jar target\/ass1-1\.0-SNAPSHOT.jar com.dsp.ass1.PDF inimage out
+rm -rf out && mvn package && hadoop jar target\/ass1-1\.0-SNAPSHOT.jar com.dsp.ass1.PDF intext out
