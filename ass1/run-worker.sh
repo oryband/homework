21eda0a9f617c8fc9ff6903e04ecd52cb5bd4311
Ory Band <oryband@gmail.com>
2014-04-09 18:41:14 +0300

    POM now uses mvn exec, and run-worker.sh uses this to run the worker; also not archiving dependencies in output jar anymore, because we're using exec plugin now.

--- ass1/run-worker.sh
+++ ass1/run-worker.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-rm -f ./*.txt ./*.html ./*.png && mvn package && java -cp target\/ass1-1\.0-SNAPSHOT.jar com.dsp.ass1.Worker ToText http://www.fbci.org/ministries/isra_docs/Passover.pdf
+rm -f ./*.txt ./*.html ./*.png && \
+mvn package && \
+mvn exec:java -Dexec.mainClass="com.dsp.ass1.Worker" \
+              -Dexec.args="ToText http://www.fbci.org/ministries/isra_docs/Passover.pdf"
