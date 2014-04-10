dfc99e22d450c2161ea40bfc1cfe7bfc37128b4c
Ory Band <oryband@gmail.com>
2014-04-10 13:57:36 +0300

    added Manager template code, nothing in it for now.

--- ass1/src/main/java/com/dsp/ass1/Manager.java
+++ ass1/src/main/java/com/dsp/ass1/Manager.java
@@ -36,38 +36,7 @@ import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 
-public class Worker {
-    private static final queueUrl = "https://queue.amazonaqs.com/3406-5707-3537/ass1"
-        private static final Logger logger = Logger.getLogger(Worker.class.getName());
-
+public class Manager {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String action = args[0],
-               url = args[1];
-
-        AmazonSQS sqs = new AmazonSQSClient(
-                new PropertiesCredentials(
-                    SimpleQueueServiceSample.class.getResourceAsStream("AwsCredentials.properties")));
-
-        ReceiveMessageRequest msgReq = new ReceiveMessageRequest(queueUrl);
-        List<Message> msgs = sqs.receiveMessage(msgReq).getMessages();
-
-        for (Message msg : msgs) {
-            System.out.println("  Message");
-            System.out.println("    MessageId:     " + message.getMessageId());
-            System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
-            System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
-            System.out.println("    Body:          " + message.getBody());
-            for (Entry<String, String> entry : message.getAttributes().entrySet()) {
-                System.out.println("  Attribute");
-                System.out.println("    Name:  " + entry.getKey());
-                System.out.println("    Value: " + entry.getValue());
-            }
-        }
-        System.out.println();
-
-        // Delete a message
-        System.out.println("Deleting a message.\n");
-        String messageRecieptHandle = messages.get(0).getReceiptHandle();
-        sqs.deleteMessage(new DeleteMessageRequest(myQueueUrl, messageRecieptHandle));
     }
 }
