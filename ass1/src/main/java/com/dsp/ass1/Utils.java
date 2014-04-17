54c678af1a553534be6288bc6a7a23993c1aa0e9
Ory Band <oryband@gmail.com>
2014-04-17 12:17:18 +0300

    logger setup moved to Utils

--- ass1/src/main/java/com/dsp/ass1/Utils.java
+++ ass1/src/main/java/com/dsp/ass1/Utils.java
@@ -1,12 +1,14 @@
 package com.dsp.ass1;
 
+import java.util.logging.Logger;
+import java.util.logging.ConsoleHandler;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.List;
-import java.util.logging.Logger;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.PropertiesCredentials;
@@ -23,9 +25,19 @@ public class Utils {
 
     private static final Logger logger = Logger.getLogger(Utils.class.getName());
 
+    // Use custom string format for logger.
+    public static void setLogger(Logger logger) {
+        ShortFormatter formatter = new ShortFormatter();
+        ConsoleHandler handler = new ConsoleHandler();
+
+        logger.setUseParentHandlers(false);
+        handler.setFormatter(formatter);
+        logger.addHandler(handler);
+    }
+
 
     public static String uploadFileToS3(AmazonS3 s3, String bucket, String path, String fileName, String info)
-            throws IOException {
+        throws IOException {
             String fileAddress = getS3FileAddress(s3, bucket, path, fileName);
             File file = createSampleFile(info);
 
