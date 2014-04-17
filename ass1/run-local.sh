4dd7a5803dcc65a99de091b203c24d5bb79864b7
Ory Band <oryband@gmail.com>
2014-04-17 21:56:02 +0300

    Local application and utils no longer have 'throws' in function defintion, since we handle exceptions on our own; Remove irrelvant code example .java files; Work-in-progress for ec2 instance launch and boot, trying to get it to work for Local Application first.

--- ass1/run-local.sh
+++ ass1/run-local.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-mvn exec:java -Dexec.mainClass="com.dsp.ass1.LocalApplication"
+mvn exec:java -Dexec.mainClass="com.dsp.ass1.LocalApplication" \
+              -Dexec.args="in"
--- ass1/src/main/java/com/dsp/ass1/Utils.java
+++ ass1/src/main/java/com/dsp/ass1/Utils.java
@@ -1,25 +1,37 @@
 package com.dsp.ass1;
 
-import java.util.logging.Logger;
-import java.util.logging.ConsoleHandler;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+
+import java.util.logging.Logger;
+import java.util.logging.ConsoleHandler;
 import java.util.List;
 
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+
 import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.auth.AWSCredentials;
+
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
+
 
 public class Utils {
 
@@ -43,12 +55,13 @@ public class Utils {
     }
 
 
-    public static String uploadFileToS3(AmazonS3 s3, String fileName, String info)
-            throws IOException {
+    public static String uploadFileToS3(AmazonS3 s3, String fileName, String info) {
         String fileAddress = getS3FileAddress(s3, fileName);
         File file = createSampleFile(info);
 
-        if (file != null || fileAddress == null) {
+        if (file == null || fileAddress == null) {
+            return null;
+        } else {
             PutObjectRequest request = new PutObjectRequest(bucket, path + fileName, file);
 
             try {
@@ -60,12 +73,11 @@ public class Utils {
             }
             return fileAddress;
         }
-        return null;
     }
 
     // TODO Write to stream and then save it to file,
     // INSTEAD of creating a file and writing to it. >:(
-    private static File createSampleFile(String info) throws IOException {
+    private static File createSampleFile(String info) {
         File file;
 
         try {
@@ -76,8 +88,15 @@ public class Utils {
         }
 
         file.deleteOnExit();
-        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
-        writer.write(info);
+
+        Writer writer;
+        try {
+            writer = new OutputStreamWriter(new FileOutputStream(file));
+            writer.write(info);
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
+            return null;
+        }
 
         try {
             writer.close();
@@ -87,6 +106,7 @@ public class Utils {
 
         return file;
     }
+
 
     // Fetches S3-file url.
     public static String getS3FileAddress(AmazonS3 s3, String fileName) {
@@ -99,20 +119,20 @@ public class Utils {
             return null;
         }
 
-        return "https://s3-" + address + ".amazonaws.com/" + bucket + "/"
-        + path + fileName;
+        return "https://s3-" + address + ".amazonaws.com/" + bucket + "/" + path + fileName;
     }
+
 
     public static PropertiesCredentials loadCredentials() {
         try {
             return new PropertiesCredentials(
-                    Utils.class
-                    .getResourceAsStream("/AWSCredentials.properties"));
+                    Utils.class.getResourceAsStream("/AWSCredentials.properties"));
         } catch (IOException e) {
             logger.severe(e.getMessage());
             return null;
         }
     }
+
 
     public static void sendMessage(AmazonSQS sqs, String sqsUrl, String info) {
         try {
@@ -123,8 +143,8 @@ public class Utils {
         }
     }
 
-    public static void deleteTaskMessage(Message msg, String sqsUrl,
-            AmazonSQS sqs) {
+
+    public static void deleteTaskMessage(Message msg, String sqsUrl, AmazonSQS sqs) {
         String handle = msg.getReceiptHandle();
 
         try {
@@ -135,8 +155,8 @@ public class Utils {
         }
     }
 
-    public static List<Message> getMessages(ReceiveMessageRequest req,
-            AmazonSQS sqs) {
+
+    public static List<Message> getMessages(ReceiveMessageRequest req, AmazonSQS sqs) {
         List<Message> msgs;
 
         try {
@@ -147,5 +167,27 @@ public class Utils {
         }
 
         return msgs;
+    }
+
+
+    public static RunInstancesResult createAmiFromSnapshot(int amount, String name, AWSCredentials creds) {
+
+        AmazonEC2 ec2;
+
+        ec2 = new AmazonEC2Client(creds);
+
+        try {
+            RunInstancesRequest request = new RunInstancesRequest();
+            request.withImageId("ami-13a6bf7a")
+                .withInstanceType("t1.micro")
+                .withMinCount(amount)
+                .withMaxCount(amount);
+
+            return ec2.runInstances(request);
+
+        } catch (AmazonServiceException e) {
+            logger.severe(e.getMessage());
+            return null;
+        }
     }
 }
