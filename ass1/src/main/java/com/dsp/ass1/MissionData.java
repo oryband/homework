666cdac5a7e9942e49938761cf1839829c2ec71c
Liran Oz <liranoz12@gmail.com>
2014-04-22 21:08:55 +0300

    Adding StringBuilder and some functions

--- ass1/src/main/java/com/dsp/ass1/MissionData.java
+++ ass1/src/main/java/com/dsp/ass1/MissionData.java
@@ -2,30 +2,43 @@ package com.dsp.ass1;
 
 public class MissionData  {
         private int number;
-        private String info;
+        private StringBuilder info;
+
 
         public MissionData(int number) {
             this.number = number;
-            this.info = "";
+            this.info = new StringBuilder();
         }
+
 
         public void setNumber(int number) {
             this.number = number;
         }
 
-        public void setInfo(String info) {
-            this.info = new String (info);
-        }
 
         public int getNumber() {
             return number;
         }
 
+
         public void decrease() {
             this.number--;
         }
 
+
         public String getInfo() {
-            return new String (info);
+            return info.toString();
+        }
+
+
+        // create the line for each pdf in the success results file.
+        public void stringAppendSucc(String[] split) {
+            info.append("\n<" + split[1] + "> :  " + split[2] + "  " + split[3]);
+        }
+
+
+        // create the line for each pdf in the success results file.
+        public void stringAppendFailed(String[] split) {
+            info.append("\n<" + split[1] + "> :  " + split[2] + "  < " + split[3] + " >");
         }
 }
