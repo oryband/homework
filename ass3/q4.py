0589725aaf2934d17727418edc5dc386a76de46d
Ory Band <oryband@gmail.com>
2014-05-11 15:54:37 +0300

    set default behaviour for q4.

--- ass3/q4.py
+++ ass3/q4.py
@@ -121,8 +121,8 @@ def simulate_skin_with_svm(data_size=None, train_params='-s 0 -t 0'):
 
 
 if __name__ == '__main__':
-    # simulate_seperable(data_size=10000)
+    simulate_seperable(data_size=10000)
     # simulate_increasing(data_size=10000, margin=0.7, max_iter=1000, learning_rate=0.01, steps=5)
     # simulate_skin(steps=10, max_iter=1000, learning_rate=0.1)
     # convert_seperable_to_svm(data_size=10000, margin=0)
-    simulate_skin_with_svm(data_size=50000, train_params='-s 0 -t 0')
+    # simulate_skin_with_svm(data_size=50000, train_params='-s 0 -t 0')
