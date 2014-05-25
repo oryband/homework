6a6fca9a07f499c9b2fa0c262c71bcddeaf83e11
Ory Band <oryband@gmail.com>
2014-05-25 13:36:12 +0300

    Created a new server object which wraps net.Server object, and added functino stubs to it (needs to be implemented).

--- Exercise 4/part2/myHttp.js
+++ Exercise 4/part2/myHttp.js
@@ -1,8 +1,47 @@
 var net = require('net');
 
-exports.createStaticHttpServer = function (rootFolder) {
+var Server = function (rootFolder) {
     'use strict';
 
-    return net.createServer({}, function(c) {
+    var serverStarted = false,
+        startDate = null,
+        serverPort = null,
+        serverCurrentRequests = 0,  // TODO: update on connection open & close.
+        serverTotalRequests = 0,
+        serverSuccessfulRequests = 0;
+
+    // TODO
+    var netServer = net.createServer(function(c) {
     });
+
+
+    return {
+        start: function (port) {
+            netServer.listen(port, function (c) {
+            });
+
+            serverStarted = true;
+            startDate = new Date().toLocaleDateString();
+            serverPort = port;
+        },
+
+        stop: function () {
+            netServer.close();
+        },
+
+        status: function () {
+            return {
+                isStarted: serverStarted,
+                startedDate: startDate,
+                port: serverPort,
+                numOfCurrentRequests: serverCurrentRequests,
+                percentageOfSuccesfulRequests: serverTotalRequests === 0 ? 100 : Math.floor(serverSuccessfulRequests * 100 / serverTotalRequests)
+            };
+        }
+    };
+};
+
+exports.createStaticHttpServer = function (rootFolder) {
+
+
 };
