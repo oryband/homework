89e808d3f0dc0f19153df2ab13318a59743deba2
Dvir Azulay <dvir.azulay@gmail.com>
2014-05-26 13:23:21 +0300

    Tester now takes command line arguments - node tester.js [skip-tests] [server timeout]. Skip-tests is either yes/no and server timeout is the time in ms until the server should die

--- Exercise 4/part2/tester.js
+++ Exercise 4/part2/tester.js
@@ -16,43 +16,54 @@ var TEST_PORT = 3008;
 server.start(TEST_PORT);
 setTimeout(function () {
     server.stop();
-}, 10000);
+}, process.argv[3] || 10000);
 
-// print the server status on init
+// execute tests
+while (true) {
 
-var serverStatus = server.status();
-// assert(serverStatus.isStarted === true, 'Server should have been started by now');
-// assert(serverStatus.port === TEST_PORT, 'Server is running on the wrong port');
-// assert(serverStatus.numOfCurrentRequests === 0, 'Server should report 0 requests on init');
-// assert(serverStatus.percentageOfSuccesfulRequests === 100, 'Server should report 100% success rate on init');
+  // if the second command line argument is yes, we should skip running 
+  // tests and just launch the server. default: run tests
+  if ((process.argv[2] || "no") === "yes") {
+    break;
+  }
 
-var req;
+  // test the server status on init
+  var serverStatus = server.status();
+  assert(serverStatus.isStarted === true, 'Server should have been started by now');
+  assert(serverStatus.port === TEST_PORT, 'Server is running on the wrong port');
+  assert(serverStatus.numOfCurrentRequests === 0, 'Server should report 0 requests on init');
+  assert(serverStatus.percentageOfSuccesfulRequests === 100, 'Server should report 100% success rate on init');
 
-req = http.request({
-    hostname: 'localhost',
-    port: TEST_PORT,
-    path: '/status',
-    method: 'GET'
-  }, function(res){
-  assert(res.httpVersion === '1.1', 'Wrong HTTP version received for /status request (Got '+res.httpVersion+')');
-  assert(res.statusCode === 200, 'Wrong status code received for /status request (Got '+res.statusCode+')');
-  assert(res.headers['content-type'] === 'text/html', '/status request should return text/html content (Got '+res.headers['content-type']+')');
-});
-req.on('error', function(e) {
-  console.log('problem with /status request: ' + e.message);
-});
-req.end();
+  var req;
 
-req = http.request({
-    hostname: 'localhost',
-    port: TEST_PORT,
-    path: '/status',
-    method: 'PUT'
-  }, function(res){
-  assert(res.httpVersion === '1.1', 'Wrong HTTP version received for /status request (Got '+res.httpVersion+')');
-  assert(res.statusCode === 405, 'Wrong status code received for a method not allowed request (Got '+res.statusCode+')');
-});
-req.on('error', function(e) {
-  console.log('problem with PUT /status BAD request: ' + e.message);
-});
-req.end();
+  req = http.request({
+      hostname: 'localhost',
+      port: TEST_PORT,
+      path: '/status',
+      method: 'GET'
+    }, function(res){
+    assert(res.httpVersion === '1.1', 'Wrong HTTP version received for /status request (Got '+res.httpVersion+')');
+    assert(res.statusCode === 200, 'Wrong status code received for /status request (Got '+res.statusCode+')');
+    assert(res.headers['content-type'] === 'text/html', '/status request should return text/html content (Got '+res.headers['content-type']+')');
+  });
+  req.on('error', function(e) {
+    console.log('problem with /status request: ' + e.message);
+  });
+  req.end();
+
+  req = http.request({
+      hostname: 'localhost',
+      port: TEST_PORT,
+      path: '/status',
+      method: 'PUT'
+    }, function(res){
+    assert(res.httpVersion === '1.1', 'Wrong HTTP version received for /status request (Got '+res.httpVersion+')');
+    assert(res.statusCode === 405, 'Wrong status code received for a method not allowed request (Got '+res.statusCode+')');
+  });
+  req.on('error', function(e) {
+    console.log('problem with PUT /status BAD request: ' + e.message);
+  });
+  req.end();
+
+  break;
+}
