6ecf6e4d88536790049650675958251a20dd09bb
Ory Band <oryband@gmail.com>
2014-05-30 16:23:03 +0300

    Moving to using scikit: removed adaboost.py and perceptron submodule, and updated data.py to convert sking-segmentation dataset to libsvm format (scikit supports it)

--- ass4/data.py
+++ ass4/data.py
@@ -1,63 +1,8 @@
 #!/usr/bin/env python
-"""Data set methods."""
+"""Dataset methods."""
 
 import csv
-
-from random import shuffle, random, uniform, choice
-from math import pi, sin, cos, sqrt
-
-
-def read_data(path, features=[], delim='\t'):
-    """Open training file and return a float samples, labels.
-
-    Each line is a vector of floats.
-    The last value is a label, which is fixed from 1/2 to 1/-1.
-    Returned samples are randomly scrambled.
-
-    path: Data file path.
-    features: List of features indexes to extract, and abandon all others.
-              If empty, extract all.
-    """
-    with open(path, 'rb') as f:
-        data = [
-            [float(xi) for xi in x] for x in csv.reader(f, delimiter=delim)
-        ]
-
-    # Extract desired features.
-    data = [[x[i] for i in features] for x in data] if features != [] else data
-
-    # Fix labels from 1/0 to 1/-1
-    data = [x[:-1] + [1] if x[-1] == 1 else x[:-1] + [-1] for x in data]
-
-    shuffle(data)
-
-    return data[:-1], data[-1]
-
-
-def generate_sphere_data(size, margin=0.2, radius=1):
-    """Generate a 2d circle dataset.
-
-    The center is randomly classified, and the borders are strictly classified.
-    """
-    samples = []
-    labels = []
-    for _ in xrange(size):
-        t = 2*pi*random()
-        r = sqrt(uniform(0, radius))
-        x1, x2 = r*cos(t), r*sin(t)
-
-        loc = x1+x2
-        if loc >= margin:
-            label = 1
-        elif loc < -margin:
-            label = -1
-        else:
-            label = choice((-1, 1))
-
-        samples.append([x1+1, x2+1])
-        labels.append(label)
-
-    return samples, labels
+from random import shuffle
 
 
 def split_list(l, ratio=0.75):
@@ -70,33 +15,59 @@ def split_list(l, ratio=0.75):
     return l[:i], l[i:]
 
 
-def convert_data_to_libsvm(data, path):
-    """Convert data matrix to libsvm format."""
-    with open(path, 'wb') as f:
-        for x in data:
-            f.write('%d' % x[-1])  # Write label.
+def read_data(path, features=None, delim='\t'):
+    """Open training file and return float samples and labels.
 
-            for i in xrange(len(x) - 1):  # Write features.
-                if (x[i] == int(x[i])):
-                    f.write(' %d:%d' % (i+1, x[i]))
-                else:
-                    f.write(' %d:%f' % (i+1, x[i]))
+    Each line is a vector of floats.
+    The last value is a label, which is fixed from 1/2 to 1/-1.
+    Returned samples and labels are randomly scrambled.
+
+    path: Data file path.
+    features: List of features indexes to extract, and abandon all others.
+              If empty, extract all.
+    """
+    with open(path, 'rb') as f:
+        data = [
+            [float(xi) for xi in x] for x in csv.reader(f, delimiter=delim)
+        ]
+
+    # Extract desired features, or all of them if no features were given.
+    data = [[x[i] for i in features] for x in data] if features else data
+
+    # Fix labels from 1/2 to 1/-1
+    data = [x[:-1] + [1] if x[-1] == 1 else x[:-1] + [-1] for x in data]
+
+    # Randomize data.
+    shuffle(data)
+
+    return data[:-1], data[-1]
+
+
+def convert_dataset_to_libsvm(samples, labels, path):
+    """Convert samples and labels to libsvm format."""
+    with open(path, 'wb') as f:
+        for sample, label in zip(samples, labels):
+            f.write(label)  # Write samble's label.
+
+            # Write sample's features.
+            for i, feature in enumerate(sample, 1):  # Write features.
+                if feature.isdigit():  # If string can be converted to int.
+                    f.write(' %d:%d' % (i, feature))
+                else:  # String is float.
+                    f.write(' %d:%f' % (i, sample))
 
             f.write('\n')
 
 
-def convert_seperable_to_svm(data_size=10000, margin=0, radius=1):
-    """Convert seperable data to libsvm format."""
-    data = generate_sphere_data(4000, margin=0.7, radius=radius)
-    convert_data_to_libsvm(data, 'seperable.svm')
-
-
 def convert_skin_to_svm(data_size=None):
-    """Convert skin data to libsvm format."""
-    data = read_data('Skin_NonSkin.txt')
+    """Convert UCI's skin dataset to libsvm format."""
+    samples, labels = read_data('skin.txt')
     if data_size is not None:
-        data = data[:data_size]
+        samples = samples[:data_size]
+        labels = labels[:data_size]
 
-    train_data, test_data = split_list(data, 0.75)
-    convert_data_to_libsvm(train_data, 'skin_train.svm')
-    convert_data_to_libsvm(test_data, 'skin_test.svm')
+    train_samples, test_samples = split_list(samples, 0.75)
+    train_labels, test_labels = split_list(labels, 0.75)
+
+    convert_dataset_to_libsvm(train_samples, train_labels, 'skin-train.libsvm')
+    convert_dataset_to_libsvm(test_samples, test_labels, 'skin-test.libsvm')
