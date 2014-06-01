c896f9a587bc98218e746b5a2780965e8506b314
Ory Band <oryband@gmail.com>
2014-06-01 18:44:44 +0300

    Removed redundant code in Count.java

--- ass2/src/main/java/com/dsp/ass2/Count.java
+++ ass2/src/main/java/com/dsp/ass2/Count.java
@@ -17,134 +17,79 @@ import org.apache.hadoop.io.LongWritable;
 
 public class Count {
 
+    private final static String ngramDelim = " ",
+            wordHeader = "*";
+
+    // Sum all members in list.
+    private static int sumValues(Iterable<IntWritable> values) {
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        return sum;
+    }
+
+
     public static class MapClass extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-        private final static IntWritable one = new IntWritable(1);
+        private final IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        // map function emit 5 times for each 5-gram : 4 times for the center word with the other words,
-        // and one time (with '*' as word) to calculate the instance of the word itself.
+        // Emit 5 times for each 5-gram:
+        // 1 time for  <w,c(w)> , emitted as { <w,*> : c(w) }
+        // 4 times for each <<w,wi>, c(w,wi)>, i=1..4 , emitted as { <w,wi> : c(w), c(w,wi) }
         @Override
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            String[] arr = value.toString().split(" ");
+        public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
 
-            switch (arr.length) {
+            // Split n-gram line into words, and handle <w,wi> pairs.
+            String[] words = value.toString().split(ngramDelim);
+            int center, i;
+            if (words.length > 0) {
+                center = words.length / 2;  // Main (central) word index.
 
-                case 1:  //  just counting to word.
-                    word.set(arr[0] + "\t*");
-                    context.write(word, one);
+                word.set(words[center] + Utils.delim + wordHeader);
+                context.write(word, one);
 
-                    break;
-
-                case 2:  // consider the pair.
-                    word.set(arr[0] + "\t*");
-                    context.write(word, one);
-
-                    word.set(arr[0] + "\t" + arr[1]);
-                    context.write(word, one);
-
-                    break;
-
-                case 3: // choose the middle word and create its context
-                    word.set(arr[1] + "\t*");
-                    context.write(word, one);
-
-                    for (int i = 0 ; i < 3 ; i++) {
-                        if (i != 1) {
-                            word.set(arr[1] + "\t" + arr[i]);
-                            context.write(word, one);
-                        }
+                for (i=0 ; i < words.length; i++) {
+                    if (i != center) {
+                        word.set(words[center] + Utils.delim + words[i]);
+                        context.write(word, one);
                     }
-
-                    break;
-
-                case 4: // choose the third word and create its context
-
-                    word.set(arr[2] + "\t*");
-                    context.write(word, one);
-
-                    for (int i = 0 ; i < 4 ; i++) {
-                        if (i != 2) {
-                            word.set(arr[2] + "\t" + arr[i]);
-                            context.write(word, one);
-                        }
-                    }
-
-                    break;
-
-                case 5:
-                    word.set(arr[2] + "\t*");
-                    context.write(word, one);
-
-                    for (int i = 0 ; i < 5 ; i++) {
-                        if (i != 2) {
-                            word.set(arr[2] + "\t" + arr[i]);
-                            context.write(word, one);
-                        }
-                    }
-
-                    break;
-
-                default:
-                    break;
+                }
             }
         }
     }
 
 
-    // Combiner class just summing to values before sending it.
     public static class CombineClass extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable sumWrt = new IntWritable();
+
+        // Sum every identical count values before sending to reducer.
         @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable value : values) {
-                sum += value.get();
-            }
-            context.write(key, new IntWritable(sum));
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+            sumWrt.set(sumValues(values));
+            context.write(key, sumWrt);
         }
     }
-
 
 
     public static class ReduceClass extends Reducer<Text,IntWritable,Text,Text> {
-        private int w1sum = 0;
+        private int cw = 0;  // c(w)
 
-        /* public void setup(Context context){
-        Configuration conf = context.getConfiguration();
-        JobConf jobConf = new JobConf(conf);
-        JobClient client;
-        try {
-            jobID = jobConf.get("mapred.job.id");
-            System.out.println(jobID);
-            client = new JobClient(jobConf);
-            RunningJob job = client.getJob(JobID.forName(jobID));
-
-            if (job == null) {
-                System.out.println("No job with ID found " + jobID);
-            } else {
-                Counters counters = job.getCounters();
-                N = counters.getCounter(MapClass.Counter.count);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    } */
-
+        // Slit <w,wi> key and count both <w,*> and <w,wi> cases.
         @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
 
-            int sum = 0;
-            for (IntWritable value : values) {
-                sum += value.get();
-            }
-
-            if (key.toString().split("\t")[1].equals("*")) {
-                w1sum = sum;
-            }
-
-            else {
-                String val = Integer.toString(sum) + "\t" + Integer.toString(w1sum);
+            // If key is <w,*>: Write c(w),
+            // Else key is <w,wi>: Write { c(w), c(w,wi) }
+            int sum = sumValues(values);
+            if (key.toString().split(Utils.delim)[1].equals(wordHeader)) {
+                cw = sum;
+            } else {
+                String val = Integer.toString(sum) + Utils.delim + Integer.toString(cw);
                 context.write(key, new Text(val));
             }
         }
@@ -152,9 +97,11 @@ public class Count {
 
 
     public static class PartitionerClass extends Partitioner<Text, IntWritable> {
+        // TODO Do this in a smarter way.
         @Override
         public int getPartition(Text key, IntWritable value, int numPartitions) {
-            return getLanguage(new Text(key.toString().split("\t")[0])) % numPartitions;
+            return getLanguage(new Text(
+                        key.toString().split(Utils.delim)[0])) % numPartitions;
         }
 
         private int getLanguage(Text key) {
@@ -170,18 +117,24 @@ public class Count {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        conf.set("mapred.reduce.slowstart.completed.maps", "1");
         //conf.set("mapred.map.tasks","10");
         //conf.set("mapred.reduce.tasks","2");
-        Job job = new Job(conf, "word count");
+        Job job = new Job(conf, "Count");
+
         job.setJarByClass(Count.class);
         job.setMapperClass(MapClass.class);
         job.setPartitionerClass(PartitionerClass.class);
         job.setCombinerClass(CombineClass.class);
         job.setReducerClass(ReduceClass.class);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+        // TODO change args to 1,2 when testing on amazon ecr.
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
