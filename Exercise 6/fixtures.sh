e01b2fb7538edfefb32a0ca7cd0d962756edfd4e
Dvir Azulay <dvir.azulay@gmail.com>
2014-06-16 02:05:24 +0300

    Fixed mongo db name to bitsplease and fixed an issue with date importing

--- Exercise 6/fixtures.sh
+++ Exercise 6/fixtures.sh
@@ -2,5 +2,5 @@
 
 # Import fixtures from josn files to mongo.
 
-mongoimport -d db -c users ./fixtures/users.json
-mongoimport -d db -c mails ./fixtures/mails.json
+mongoimport -d bitsplease -c users ./fixtures/users.json
+mongoimport -d bitsplease -c mails ./fixtures/mails.json
