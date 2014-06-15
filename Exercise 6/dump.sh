5e3d3e190a3706900867194f0db065a33d482780
Ory Band <oryband@gmail.com>
2014-06-15 17:29:26 +0300

    Updated schemas to use only User and Mail models, no Contact model needed.
    * Contact schema was merged into User (just moved its fields into User's).
    * Added helper-queries to get user by username, id, mails by user. etc.
    * In addition, most helper-queries now use a single currying function for the callback.
    * Updated fixtures for no schema structure.
    * Also updated dump.sh file to dump according to this schema.

--- Exercise 6/dump.sh
+++ Exercise 6/dump.sh
@@ -2,6 +2,5 @@
 
 # Dump documents from mongo to json file.
 
-mongoexport -d db -c contacts > schemas/fixtures/contacts.json
-mongoexport -d db -c users > schemas/fixtures/users.json
-mongoexport -d db -c mails > schemas/fixtures/mails.json
+mongoexport -d bitsplease -c users > schemas/fixtures/users.json
+mongoexport -d bitsplease -c mails > schemas/fixtures/mails.json
--- Exercise 6/schemas/schemas.js
+++ Exercise 6/schemas/schemas.js
@@ -1,29 +1,23 @@
 'use strict';
 
-var mg = require('mongoose');
+var mg = require('mongoose'),
+    db = mg.connect('mongodb://localhost/bitsplease');
 
 // Shorthand variables.
 var oId = mg.Schema.Types.ObjectId;
 
 
-// Contact details.
-exports.Contact = mg.model('Contact', new mg.Schema({
-    address: String,
+exports.User = mg.model('User', new mg.Schema({
+    username: String,  // User's email address is 'username@bitsplease.com'
+    password: String,
     firstName: { type: String, default: '' },
     lastName: { type: String, default: '' },
     age: { type: Number, default: null }
 }));
 
 
-exports.User = mg.model('User', new mg.Schema({
-    name: String,
-    password: String,
-    contact: { type: oId, ref: 'Contact' },
-}));
-
-
 exports.Mail = mg.model('Mail', new mg.Schema({
-    from: { type: oId, ref: 'Contact' },
+    from: { type: oId, ref: 'User' },
     to: { type: oId, ref: 'User' },
     date: { type: Date, default: Date.now },
     subject: String,
@@ -31,8 +25,9 @@ exports.Mail = mg.model('Mail', new mg.Schema({
 }));
 
 
-exports.getMailsByUser = function (user, callback) {
-    exports.Mail.find({to: user}).exec(function (err, result) {
+// Currying function which executes callback on query result.
+function execCallback(callback) {
+    return function (err, result) {
         if (err) {
             console.error(err);
             return;
@@ -41,5 +36,25 @@ exports.getMailsByUser = function (user, callback) {
         if (callback) {
             callback(result);
         }
-    });
+    }
+}
+
+
+exports.getUserById = function (id, callback) {
+    exports.User.findById(id).exec(execCallback(callback));
+};
+
+
+exports.getUserByUsername = function (username, callback) {
+    exports.User.findOne({username: username}).exec(execCallback(callback));
+};
+
+
+exports.getMailById = function (id, callback) {
+    exports.Mail.findById(id).exec(execCallback(callback));
+};
+
+
+exports.getMailsToUser = function (user, callback) {
+    exports.Mail.find({to: user}).exec(execCallback(callback));
 };
