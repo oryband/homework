9055f92009ec38e26bf2ae130cc81de7a500fff3
Ory Band <oryband@gmail.com>
2014-07-03 21:16:02 +0300

    Finished Pairs.java, untested.

--- ass3/src/main/java/com/dsp/ass3/Pairs.java
+++ ass3/src/main/java/com/dsp/ass3/Pairs.java
@@ -1,4 +1,4 @@
-package com.dsp.ass2;
+package com.dsp.ass3;
 
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -15,11 +15,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
-public class Join {
+public class Pairs {
 
-    private static final Logger logger = Utils.setLogger(Logger.getLogger(Join.class.getName()));
+    private static final Logger logger = Utils.setLogger(Logger.getLogger(Pairs.class.getName()));
 
-    // Write { <w1,w2> : decade, w1, c(w1), c(w1,w2) }
+    // Write { (N1, N2), * : True/False, HypernymIndex }  -- (Hypernym Index in pair array).
     public static class MapClass extends Mapper<LongWritable, Text, Text, Text> {
 
         private Text newKey = new Text(),
@@ -29,21 +29,22 @@ public class Join {
         public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
 
-            // Fetch words from value.
+            // Fetch pairs and tag from value.
             String[] words = value.toString().split(Utils.delim);
-            String decade = words[0],
-                   w1 = words[1],
-                   w2 = words[2],
-                   cW1 = words[3],
-                   cW1W2 = words[4];
+            String hyponym = words[0],
+                   hypernym = words[1],
+                   related = words[2];  // True/False
 
-            newValue.set(w1 + Utils.delim + cW1 + Utils.delim + cW1W2);
 
             // Emit key by lexicographical order.
-            if (w1.compareTo(w2) < 0) {
-                newKey.set(decade + Utils.delim + w1 + Utils.delim + w2);
+            if (hyponym.compareTo(hypernym) < 0) {
+                newKey.set(hyponym + Utils.delim + hypernym + Utils.delim + Utils.joinStart);
+                // 1 is the hypernym index in the pair. That is, the second word when sorted lexi.
+                newValue.set(related + Utils.delim + 1);
             } else {
-                newKey.set(decade + Utils.delim + w2 + Utils.delim + w1);
+                newKey.set(hypernym + Utils.delim + hyponym + Utils.delim + Utils.joinStart);
+                // 1 is the hypernym index in the pair. That is, the first word when sorted lexi.
+                newValue.set(related + Utils.delim + 0);
             }
 
             context.write(newKey, newValue);
@@ -51,7 +52,7 @@ public class Join {
     }
 
 
-    // Partition by 'decade + w1 + w2' hash code.
+    // Partition by key hash code.
     public static class PartitionerClass extends Partitioner<Text, Text> {
         @Override
         public int getPartition(Text key, Text value, int numPartitions) {
@@ -61,49 +62,24 @@ public class Join {
     }
 
 
-    // For every <w1,w2> - Write { <decade, w1 ,w2> : c(w1), c(w2), c(w1,w2) }
+    // Reducer just passes on data.
     public static class ReduceClass extends Reducer<Text, Text, Text, Text> {
-
-        private Text newValue = new Text();
 
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
-            String cW1 = "0", cW2 = "0", cW1W2 = "0";
 
-            // Fetch w1, w2, c(w1), c(w2), c(w1,w2).
-            String[] parseKey = key.toString().split(Utils.delim);
-            String w1 = parseKey[1],
-                   w2 = parseKey[2];
-
-            String[] counters;
-            for (Text value : values) {
-                counters = value.toString().split(Utils.delim);
-
-                if (counters[0].equals(w1)) {
-                    cW1 = counters[1];
-                    cW1W2 = counters[2];
-                }
-
-                // If w1 = w2, prevent cases where c(w2) = 0.
-                if (counters[0].equals(w2)) {
-                    cW2 = counters[1];
-                }
-            }
-
-            newValue.set(cW1 + Utils.delim + cW2 + Utils.delim + cW1W2);
-            context.write(key, newValue);
+            context.write(key, values.iterator().next());
         }
     }
 
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        conf.set("mapred.reduce.slowstart.completed.maps", "1");
 
-        Job job = new Job(conf, "Join");
+        Job job = new Job(conf, "Pairs");
 
-        job.setJarByClass(Join.class);
+        job.setJarByClass(Pairs.class);
         job.setMapperClass(MapClass.class);
         job.setPartitionerClass(PartitionerClass.class);
         job.setReducerClass(ReduceClass.class);
@@ -115,13 +91,6 @@ public class Join {
         FileOutputFormat.setOutputPath(job, new Path(args[Utils.argInIndex + 1]));
 
         boolean result = job.waitForCompletion(true);
-
-        // Get totalRecords parameter of Count step, and add this (Join) step total records.
-        if (result) {
-            long totalRecords = job.getCounters().findCounter("org.apache.hadoop.mapred.Task$Counter", "MAP_OUTPUT_RECORDS").getValue();
-            String info = "totalrecords" + Utils.delim + Long.toString(totalRecords);
-            Utils.uploadToS3(info, Utils.joinOutput + Utils.countersFileName);
-        }
 
         System.exit(result ? 0 : 1);
     }
