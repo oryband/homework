f592ff82671ac6c4a039cb9015e344ec7d8ae88a
Ory Band <oryband@gmail.com>
2014-07-08 16:25:22 +0300

    Removed lucene from ass3

--- ass3/update-lucene.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/hadoop
-wget https://s3.amazonaws.com/ory-dsp-ass3/lucene/lucene-4.8.1.tgz
-tar -xzf lucene-4.8.1.tgz
-rm -f lib/lucene-*.jar
-find lucene-4.8.1 | grep lucene- | grep jar$ | xargs -I {} cp {} lib/
