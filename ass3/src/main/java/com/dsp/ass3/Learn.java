a877c80f83d657824ca1b066e1db78bd2d254af0
Ory Band <oryband@gmail.com>
2014-07-12 20:15:48 +0300

    Started working on WEKA Learn.java

---
+++ ass3/src/main/java/com/dsp/ass3/Learn.java
@@ -0,0 +1,11 @@
+// import weka.core.Instances;
+// import weka.core.converters.ConverterUtils.DataSource;
+
+
+// public class Learn {
+//     public static void main(String[] args) throws Exception {
+//         DataSource source = new DataSource("");
+//         Instances data = source.getDataSet();
+//         data.setClassIndex(0);
+//     }
+// }
