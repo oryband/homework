99671d426829fcc7daf4f5ddc2d6a7552d5c19ce
Ory Band <oryband@gmail.com>
2014-07-12 20:49:01 +0300

    merge.sh script not prints class @ATTRIBUTE

--- ass3/merge-data.sh
+++ ass3/merge-data.sh
@@ -2,7 +2,8 @@
 
 DIR=$1
 
-echo "@RELATION deptrees" >> $DIR/data
-cat $DIR/headers >> $DIR/data
-echo "@DATA" >> $DIR/data
-cat $DIR/vectors >> $DIR/data
+echo "@RELATION deptrees" > $DIR/data.arff
+echo "@ATTRIBUTE related {true,false}" >> $DIR/data.arff
+cat $DIR/headers >> $DIR/data.arff
+echo "@DATA" >> $DIR/data.arff
+cat $DIR/vectors >> $DIR/data.arff
