82cc3ab0f6089483175f1c83cfef7d471ebd088b
Liran Oz <liranoz12@gmail.com>
2014-07-12 20:49:43 +0300

    start learn file

--- ass3/src/main/java/com/dsp/ass3/Learn.java
+++ ass3/src/main/java/com/dsp/ass3/Learn.java
@@ -1,11 +1,40 @@
-// import weka.core.Instances;
-// import weka.core.converters.ConverterUtils.DataSource;
+package com.dsp.ass3;
+
+import java.util.Random;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 
-// public class Learn {
-//     public static void main(String[] args) throws Exception {
-//         DataSource source = new DataSource("");
-//         Instances data = source.getDataSet();
-//         data.setClassIndex(0);
-//     }
-// }
+ public class Learn {
+     public static void main(String[] args) throws Exception {
+         DataSource source = new DataSource("in/test/data.arff");
+         Instances data = source.getDataSet();
+         data.setClassIndex(0);
+
+         int folds = 10, seed = 10;
+
+         Random rand = new Random(seed);   // create seeded number generator
+         Instances randData = new Instances(data);
+         randData.randomize(rand);
+
+         Evaluation eval = new Evaluation(randData);
+
+         for (int n = 0; n < folds; n++) {
+             Instances train = randData.trainCV(folds, n);
+             Instances test = randData.testCV(folds, n);
+
+             // build and evaluate classifier
+             Classifier cModel = (Classifier) new NaiveBayes();
+             cModel.buildClassifier(train);
+             eval.evaluateModel(cModel, test);
+           }
+
+         // Print the result
+         String strSummary = eval.toSummaryString();
+         System.out.println(strSummary);
+     }
+ }
