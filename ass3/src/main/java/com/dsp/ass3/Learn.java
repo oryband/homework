855b08bdb506ac8595320bc37b816505afaabdab
Ory Band <oryband@gmail.com>
2014-07-13 13:21:19 +0300

    Learn can now read from s3, and added docs.

--- ass3/src/main/java/com/dsp/ass3/Learn.java
+++ ass3/src/main/java/com/dsp/ass3/Learn.java
@@ -1,6 +1,10 @@
 package com.dsp.ass3;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Random;
+import java.util.logging.Logger;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
@@ -8,33 +12,117 @@ import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 
- public class Learn {
-     public static void main(String[] args) throws Exception {
-         DataSource source = new DataSource("in/test/data.arff");
-         Instances data = source.getDataSet();
-         data.setClassIndex(0);
 
-         int folds = 10, seed = 10;
+public class Learn {
+    private static int classIndex = 0;
 
-         Random rand = new Random(seed);   // create seeded number generator
-         Instances randData = new Instances(data);
-         randData.randomize(rand);
+    private static final Logger logger = Utils.setLogger(Logger.getLogger(Learn.class.getName()));
 
-         Evaluation eval = new Evaluation(randData);
+    private static InputStream readSource(String protocol, String path) {
+        InputStream is;
 
-         for (int n = 0; n < folds; n++) {
-             Instances train = randData.trainCV(folds, n);
-             Instances test = randData.testCV(folds, n);
+        // Read path from S3 and return input stream.
+        if (protocol.toLowerCase().equals("s3")) {
+            // Init S3 connection.
+            AWSCredentials creds = Utils.loadCredentials();
+            if (creds == null) {
+                logger.severe("Couldn't load credentials.");
+                return null;
+            }
 
-             // build and evaluate classifier
-             Classifier cModel = (Classifier) new NaiveBayes();
-             cModel.buildClassifier(train);
-             eval.evaluateModel(cModel, test);
-           }
+            AmazonS3 s3 = Utils.createS3(creds);
 
-         // Print the result
-         String strSummary = eval.toSummaryString();
-         System.out.println(strSummary);
-     }
- }
+            // Download file as stream.
+            logger.info("Downloading stream from S3.");
+
+            is = Utils.readFromS3(s3, path);
+            if (is == null) {
+                logger.severe("Error reading input file.");
+                return null;
+            }
+
+            return is;
+        } else {
+            // Else read and return local file as stream.
+            File file = new File(path);
+            try {
+                is = new FileInputStream(file);
+            } catch (Exception e) {
+                logger.severe("Error opening local file.");
+                return null;
+            }
+
+            return is;
+        }
+    }
+
+
+    private static Instances readData(String protocol, String path) {
+        logger.info("Reading file: " + path);
+
+        InputStream is = readSource(protocol, path);
+        if (is == null) {
+            return null;
+        }
+
+        DataSource source = new DataSource(is);
+
+        // Parse source.
+        logger.info("Parsing data.");
+
+        Instances data;
+        try {
+            data = source.getDataSet(classIndex);
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
+            return null;
+        }
+
+        if (data == null) {
+            logger.severe("Error parsing source.");
+            return null;
+        }
+
+        return data;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        // Read path.
+        logger.info("Reading path: " + args[1]);
+
+        // Parse data.
+        Instances data = readData(args[0], args[1]);
+        if (data == null) {
+            return;
+        }
+
+        // Randomize seed.
+        int seed = 10;
+        Instances randData = new Instances(data);
+        randData.randomize(new Random(seed));
+
+        // Read randomized data.
+        Evaluation eval = new Evaluation(randData);
+
+        // Learn and perform 10-fold validation.
+        int folds = 10;
+        for (int n=0; n < folds; n++) {
+            // Set train/test instances.
+            Instances train = randData.trainCV(folds, n),
+                      test = randData.testCV(folds, n);
+
+            // Build and evaluate classifier.
+            Classifier cModel = (Classifier) new NaiveBayes();
+            cModel.buildClassifier(train);
+            eval.evaluateModel(cModel, test);
+        }
+
+        // Print result.
+        System.out.println(eval.toClassDetailsString());
+        System.out.println(eval.toSummaryString());
+    }
+}
