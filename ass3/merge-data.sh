831b19b355e7b6e67b257c2baf272e61851d2bb3
Ory Band <oryband@gmail.com>
2014-07-24 15:57:53 +0300

    Updated in/test files and merge.sh for updated biarcs.

--- ass3/merge-data.sh
+++ ass3/merge-data.sh
@@ -2,8 +2,17 @@
 
 DIR=$1
 
+# Echo title.
 echo "@RELATION deptrees" > $DIR/data.arff
+
+# Echo first attribute (class).
 echo "@ATTRIBUTE related {true,false}" >> $DIR/data.arff
-cat $DIR/headers >> $DIR/data.arff
+
+# Echo all attributes.
+cat $DIR/attributes >> $DIR/data.arff
+
+# Echo data title.
 echo "@DATA" >> $DIR/data.arff
-cat $DIR/vectors >> $DIR/data.arff
+
+# Echo all data.
+cat $DIR/data >> $DIR/data.arff
